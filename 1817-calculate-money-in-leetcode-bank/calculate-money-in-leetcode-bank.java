b8ff496da6ebce90cd15b9635dd23e9c9d218e35
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2023-12-19 14:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (28.64%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int totalMoney(int n) {
+        int week_count = n / 7;
+        int remaining_days = n % 7;
+        
+        int total = ((week_count * (week_count - 1)) / 2) * 7; // Contribution from complete weeks for ()week count-1)
+        total += week_count * 28; // Contribution from complete weeks (additional on Mondays)
+        total += ((remaining_days * (remaining_days + 1)) / 2) + (week_count * remaining_days); // Contribution from remaining days
+        
+        return total;
+    }
+}
