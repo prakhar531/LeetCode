ad76b9d078fface9c46730f3be3767a92356a0ee
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2023-12-19 11:20:13 +0530

    Time: 1 ms (98.23%) | Memory: 43.9 MB (85.31%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,7 +1,27 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
-        return (nums[nums.length-1]*nums[nums.length-2])-(nums[0]*nums[1]);
-        
+        int[] cloneNums = Arrays.copyOf(nums, nums.length);
+        int[] fmaxmin = getMaxMin(cloneNums);
+        int[] smaxmin = getMaxMin(cloneNums);
+
+        return fmaxmin[0] * smaxmin[0] - fmaxmin[1] * smaxmin[1];
+    }
+
+    private int[] getMaxMin(int[] nums) {
+        int len = nums.length;
+        int maxi = 0, mini = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                maxi = i;
+            }
+            if (nums[i] < min && nums[i] != Integer.MIN_VALUE) {
+                min = nums[i];
+                mini = i;
+            }
+        }
+        nums[maxi] = Integer.MIN_VALUE;
+        nums[mini] = Integer.MIN_VALUE;
+        return new int[] {max, min};
     }
 }
