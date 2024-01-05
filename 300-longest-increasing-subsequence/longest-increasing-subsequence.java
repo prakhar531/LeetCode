bed611f73b88dc4143639bcb72dace90debfd05e
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-05 22:06:29 +0530

    Time: 2 ms (99.90%) | Memory: 44.1 MB (30.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,31 +1,49 @@
 class Solution {
-    public int lcs(int arr[],int nums[]){
-        int n=arr.length;
-        int m=nums.length;
-        int dp[][]=new int[n+1][m+1];
+    // public int lcs(int arr[],int nums[]){
+    //     int n=arr.length;
+    //     int m=nums.length;
+    //     int dp[][]=new int[n+1][m+1];
 
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(arr[i-1]==nums[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
+    //     for(int i=1;i<n+1;i++){
+    //         for(int j=1;j<m+1;j++){
+    //             if(arr[i-1]==nums[j-1]){
+    //                 dp[i][j]=dp[i-1][j-1]+1;
+    //             }else{
+    //                 dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+    //             }
+    //         }
+    //     }
 
-        return dp[n][m];
-    }
+    //     return dp[n][m];
+    // }
+    // public int lengthOfLIS(int[] nums) {
+    //     HashSet<Integer> set=new HashSet<>();
+    //     for(int i=0;i<nums.length;i++) set.add(nums[i]);
+
+    //     int arr[]=new int[set.size()];
+    //     int i=0;
+    //     for(int num:set) arr[i++]=num;
+        
+    //     Arrays.sort(arr);
+    //     return lcs(arr,nums);
+        
+    // }
+
     public int lengthOfLIS(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++) set.add(nums[i]);
-
-        int arr[]=new int[set.size()];
-        int i=0;
-        for(int num:set) arr[i++]=num;
-        
-        Arrays.sort(arr);
-        return lcs(arr,nums);
-        
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
+            }
+            tails[i] = x;
+            if (i == size) ++size;
+        }
+        return size;
     }
 }
