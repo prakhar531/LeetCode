4c009f0e29c739594c840abc761a6a94cae09d3e
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-12 11:29:10 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (19.43%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,20 +1,20 @@
 class Solution {
-    Set<Character> set=new HashSet<>();
-    public void inti(){
-        char ch[]={'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        for(char c: ch) set.add(c);
-    }
+    // Set<Character> set=new HashSet<>();
+    // public void inti(){
+    //     char ch[]={'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    //     for(char c: ch) set.add(c);
+    // }
     public int count(String s){
         int c=0;
         for(char ch:s.toCharArray()){
-            if(set.contains(ch)) c++;
+            if("AEIOUaeiou".indexOf(ch)!=-1) c++;
         }
         return c;
     }
     public boolean halvesAreAlike(String s) {
         String a=s.substring(0,s.length()/2);
         String b=s.substring(s.length()/2);
-        inti();
+        // inti();
         int acount=count(a);
         int bcount=count(b);
         return acount==bcount;
