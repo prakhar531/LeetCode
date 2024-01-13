fec9ccd2da5784787d37a4765eb54f1290f298af
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-13 12:17:14 +0530

    Time: 6 ms (99.74%) | Memory: 45.2 MB (28.67%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,19 +1,40 @@
 class Solution {
     public int minSteps(String s, String t) {
-        Map<Character,Integer> smap=new HashMap<>();
-        for(char ch:s.toCharArray()) smap.put(ch,smap.getOrDefault(ch,0)+1);
+        // Map<Character,Integer> smap=new HashMap<>();
+        // for(char ch:s.toCharArray()) smap.put(ch,smap.getOrDefault(ch,0)+1);
 
-        Map<Character,Integer> tmap=new HashMap<>();
-        for(char ch:t.toCharArray()) tmap.put(ch,tmap.getOrDefault(ch,0)+1);
+        // Map<Character,Integer> tmap=new HashMap<>();
+        // for(char ch:t.toCharArray()) tmap.put(ch,tmap.getOrDefault(ch,0)+1);
 
-        int ans=0;
-        for(char ch:tmap.keySet()){
-            if(!smap.containsKey(ch)){
-                ans+=tmap.get(ch);
-            }else{
-                if(tmap.get(ch)>smap.get(ch)) ans+=tmap.get(ch)-smap.get(ch);
+        // int ans=0;
+        // for(char ch:tmap.keySet()){
+        //     if(!smap.containsKey(ch)){
+        //         ans+=tmap.get(ch);
+        //     }else{
+        //         if(tmap.get(ch)>smap.get(ch)) ans+=tmap.get(ch)-smap.get(ch);
+        //     }
+        // }
+        // return ans;
+
+        int[] charFreq = new int[26];
+
+        for(char cur: s.toCharArray()) {
+            charFreq[cur-'a']++;
+        }
+
+        for(char cur: t.toCharArray()) {
+            charFreq[cur-'a']--;
+        }
+
+        int minSteps = 0;
+
+        for(int idx=0; idx < 26; idx++) {
+            if (charFreq[idx] > 0) {
+                minSteps += charFreq[idx];
             }
         }
-        return ans;
+
+        return minSteps;
+
     }
 }
