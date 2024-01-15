701d23676f659caa64887464a313d850316c7391
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-15 11:52:45 +0530

    Time: 23 ms (99.63%) | Memory: 96.7 MB (48.85%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -28,39 +28,78 @@
 //     }
 // }
 
+// class Solution {
+//     public List<List<Integer>> findWinners(int[][] matches) {
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+//         int n=matches.length;
+//         for(int i=0;i<n;i++){
+//             if(hm.containsKey(matches[i][0]))
+//                 hm.put(matches[i][0],hm.get(matches[i][0]));
+//             else
+//                 hm.put(matches[i][0],0);
+            
+//             if(hm.containsKey(matches[i][1]))
+//                 hm.put(matches[i][1],hm.get(matches[i][1])+1);
+//             else
+//                 hm.put(matches[i][1],1);
+//         }
+
+//         List<List<Integer>> list=new ArrayList<>();
+//         List<Integer> list1=new ArrayList<>();
+//         List<Integer> list2=new ArrayList<>();
+
+//         for(int key:hm.keySet()){
+//             if(hm.get(key)==0){
+//                 list1.add(key);
+//             }
+//             if(hm.get(key)==1){
+//                 list2.add(key);
+//             }
+//         }
+
+//         list.add(list1);
+//         list.add(list2);
+//         Collections.sort(list1);
+//         Collections.sort(list2);
+//         return list;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int n=matches.length;
-        for(int i=0;i<n;i++){
-            if(hm.containsKey(matches[i][0]))
-                hm.put(matches[i][0],hm.get(matches[i][0]));
-            else
-                hm.put(matches[i][0],0);
-            
-            if(hm.containsKey(matches[i][1]))
-                hm.put(matches[i][1],hm.get(matches[i][1])+1);
-            else
-                hm.put(matches[i][1],1);
-        }
+        int[] losses = new int[100001];
 
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> list1=new ArrayList<>();
-        List<Integer> list2=new ArrayList<>();
+        for (int i = 0; i < matches.length; i++) {
+            int win = matches[i][0];
+            int loss = matches[i][1];
 
-        for(int key:hm.keySet()){
-            if(hm.get(key)==0){
-                list1.add(key);
-            }
-            if(hm.get(key)==1){
-                list2.add(key);
+            if (losses[win] == 0) {
+                losses[win] = -1;
+            } 
+
+            if (losses[loss] == -1) {
+                losses[loss] = 1;
+            } else {
+                losses[loss]++;
             }
         }
 
-        list.add(list1);
-        list.add(list2);
-        Collections.sort(list1);
-        Collections.sort(list2);
-        return list;
+        List<Integer> zeroLoss = new ArrayList<>();
+        List<Integer> oneLoss = new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < losses.length; i++) {
+            if (losses[i] == -1) {
+                zeroLoss.add(i);
+            } else if (losses[i] == 1) {
+                oneLoss.add(i);
+            }
+        }
+
+        result.add(zeroLoss);
+        result.add(oneLoss);
+
+        return result;
     }
 }
