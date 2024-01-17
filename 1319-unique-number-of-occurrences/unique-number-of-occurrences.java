b998100c2c4a477ce62b382dd566f80c19535b91
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-17 10:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.39%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,14 +1,31 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:arr) map.put(num,map.getOrDefault(num,0)+1);
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int num:arr) map.put(num,map.getOrDefault(num,0)+1);
 
-        HashSet<Integer> set=new HashSet<>();
-        for(int num:map.keySet()){
-            if(!set.contains(map.get(num))){
-                set.add(map.get(num));
-            }else{
-                return false;
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int num:map.keySet()){
+        //     if(!set.contains(map.get(num))){
+        //         set.add(map.get(num));
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int[] numberOfOccurrences = new int[2001];
+        boolean[] used = new boolean[1001];
+
+        for (int i : arr) {
+            numberOfOccurrences[1000 + i]++;
+        }
+
+        for (int i : arr) {
+            int count = numberOfOccurrences[1000 + i];
+            numberOfOccurrences[1000 + i] = 0; 
+            if (count > 0) {
+                if (used[count]) return false;
+                else used[count] = true;
             }
         }
         return true;
