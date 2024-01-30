54616ca36a7c544d4787766134923d7228d094a6
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-30 20:05:47 +0530

    Time: 2 ms (99.46%) | Memory: 44.4 MB (54.70%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,34 +1,62 @@
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         Stack < Integer > stack = new Stack < > ();
+//         int a, b;
+//         for (String str: tokens) {
+//             switch (str) {
+//                 case "+":
+//                     a = stack.pop();
+//                     b = stack.pop();
+//                     stack.push(b + a);
+//                     break;
+//                 case "-":
+//                     a = stack.pop();
+//                     b = stack.pop();
+//                     stack.push(b - a);
+//                     break;
+//                 case "*":
+//                     a = stack.pop();
+//                     b = stack.pop();
+//                     stack.push(b * a);
+//                     break;
+//                 case "/":
+//                     a = stack.pop();
+//                     b = stack.pop();
+//                     stack.push(b / a);
+//                     break;
+//                 default:
+//                     stack.push(Integer.parseInt(str));
+//             }
+//         }
+//         System.out.print(stack);
+//         return stack.pop();
+//     }
+// }
+
 class Solution {
+    int pos;
     public int evalRPN(String[] tokens) {
-        Stack < Integer > stack = new Stack < > ();
-        int a, b;
-        for (String str: tokens) {
-            switch (str) {
-                case "+":
-                    a = stack.pop();
-                    b = stack.pop();
-                    stack.push(b + a);
-                    break;
-                case "-":
-                    a = stack.pop();
-                    b = stack.pop();
-                    stack.push(b - a);
-                    break;
-                case "*":
-                    a = stack.pop();
-                    b = stack.pop();
-                    stack.push(b * a);
-                    break;
-                case "/":
-                    a = stack.pop();
-                    b = stack.pop();
-                    stack.push(b / a);
-                    break;
-                default:
-                    stack.push(Integer.parseInt(str));
-            }
+        this.pos = tokens.length-1;
+        return evaluate(tokens);
+    }
+
+    public int evaluate(String[] t){
+        String token = t[pos];
+        pos--;
+        if(token.equals("+")){
+            return evaluate(t) + evaluate(t); 
+        }else
+        if(token.equals("*")){
+            return evaluate(t) * evaluate(t);
+        }else 
+        if(token.equals("-")){
+            return -evaluate(t) + evaluate(t);
+        }else
+        if(token.equals("/")){
+            int right = evaluate(t);
+            return evaluate(t) / right;
+        }else{
+            return Integer.parseInt(token);
         }
-        System.out.print(stack);
-        return stack.pop();
     }
 }
