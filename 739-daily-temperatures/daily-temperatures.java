8164fdb2dc05dd1abfebd05c91361f95e787a341
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-31 14:50:38 +0530

    Time: 6 ms (100.00%) | Memory: 60.5 MB (41.51%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -20,32 +20,66 @@
 
 
 
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         Deque<Integer> deque = new ArrayDeque<>();
+        
+//         int[] res = new int[temperatures.length];
+
+//         for (int i = temperatures.length - 1; i >= 0; --i) {
+//             if (deque.isEmpty()) {
+//                 deque.offerFirst(i);
+//                 res[i] = 0;
+//             } else {
+//                 while (!deque.isEmpty() && temperatures[i] >= temperatures[deque.peekFirst()]) {
+//                     deque.pollFirst();
+//                 }
+
+//                 if (deque.isEmpty()) {
+//                     res[i] = 0;
+//                 } else {
+//                     res[i] = deque.peekFirst() - i;
+//                 }
+
+//                 deque.offerFirst(i);
+//             }
+//         }
+
+//         return res;
+//     }
+// }
+
+// haven't checked yet, seems unnecessarily complicated
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> deque = new ArrayDeque<>();
+
+        int n = temperatures.length;
         
-        int[] res = new int[temperatures.length];
+        int hottest = 0;
+        
+        int answer[] = new int[n];
+        
+        for (int currDay = n - 1; currDay >= 0; currDay--) {
 
-        for (int i = temperatures.length - 1; i >= 0; --i) {
-            if (deque.isEmpty()) {
-                deque.offerFirst(i);
-                res[i] = 0;
-            } else {
-                while (!deque.isEmpty() && temperatures[i] >= temperatures[deque.peekFirst()]) {
-                    deque.pollFirst();
-                }
+            int currentTemp = temperatures[currDay];
 
-                if (deque.isEmpty()) {
-                    res[i] = 0;
-                } else {
-                    res[i] = deque.peekFirst() - i;
-                }
-
-                deque.offerFirst(i);
+            // hottest temp seen so far moving from the right
+            if (currentTemp >= hottest) {
+                hottest = currentTemp;
+                continue;
             }
-        }
+            
+            int days = 1;
 
-        return res;
+            while (temperatures[currDay + days] <= currentTemp) {
+                // Use information from answer to search for the next warmer day
+                days += answer[currDay + days];
+            }
+
+            answer[currDay] = days;
+        }
+        
+        return answer;
     }
 }
-
