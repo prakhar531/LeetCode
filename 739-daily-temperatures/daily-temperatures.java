3acea2c30b529d92cc3557382247be6589641571
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-31 14:48:29 +0530

    Time: 24 ms (86.25%) | Memory: 60.2 MB (49.71%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,19 +1,51 @@
+// class Solution {
+//     public int[] dailyTemperatures(int[] temp) {
+//         int n=temp.length;
+//         int ans[]=new int[n];
+//         Stack<Integer> s=new Stack<>();
+//         for(int i=n-1;i>=0;i--){
+//             while(!s.isEmpty() && temp[s.peek()]<=temp[i]){
+//                 s.pop();
+//             }
+//             if(s.isEmpty()){
+//                 ans[i]=0;
+//             }else{
+//                 ans[i]=s.peek()-i;
+//             }
+//             s.push(i);
+//         }
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
-    public int[] dailyTemperatures(int[] temp) {
-        int n=temp.length;
-        int ans[]=new int[n];
-        Stack<Integer> s=new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!s.isEmpty() && temp[s.peek()]<=temp[i]){
-                s.pop();
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        int[] res = new int[temperatures.length];
+
+        for (int i = temperatures.length - 1; i >= 0; --i) {
+            if (deque.isEmpty()) {
+                deque.offerFirst(i);
+                res[i] = 0;
+            } else {
+                while (!deque.isEmpty() && temperatures[i] >= temperatures[deque.peekFirst()]) {
+                    deque.pollFirst();
+                }
+
+                if (deque.isEmpty()) {
+                    res[i] = 0;
+                } else {
+                    res[i] = deque.peekFirst() - i;
+                }
+
+                deque.offerFirst(i);
             }
-            if(s.isEmpty()){
-                ans[i]=0;
-            }else{
-                ans[i]=s.peek()-i;
-            }
-            s.push(i);
         }
-        return ans;
+
+        return res;
     }
 }
+
