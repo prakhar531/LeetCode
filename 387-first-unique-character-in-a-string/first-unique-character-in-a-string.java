d6ba2708f0717492575e2c4c156c37c6f96be678
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-05 19:52:24 +0530

    Time: 4 ms (91.34%) | Memory: 44.4 MB (72.81%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,17 +1,31 @@
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         int fre[]=new int[26];
+//         Queue<Integer> q=new LinkedList<>();
+//         int n=s.length();
+//         for(int i=0;i<n;i++){
+//             char ch=s.charAt(i);
+//             fre[ch-'a']++;
+//             q.add(i);
+//             while(!q.isEmpty()&&fre[s.charAt(q.peek())-'a']>1){
+//                 q.remove();
+//             }
+//         }
+//         return q.isEmpty()?-1:q.peek();
+        
+//     }
+// }
 class Solution {
     public int firstUniqChar(String s) {
         int fre[]=new int[26];
-        Queue<Integer> q=new LinkedList<>();
         int n=s.length();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
             fre[ch-'a']++;
-            q.add(i);
-            while(!q.isEmpty()&&fre[s.charAt(q.peek())-'a']>1){
-                q.remove();
-            }
         }
-        return q.isEmpty()?-1:q.peek();
-        
+        for(int i=0;i<n;i++){
+            if(fre[s.charAt(i)-'a']==1) return i;
+        }
+        return -1;      
     }
 }
