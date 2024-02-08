e2388a83397880de96b8e1c411e5788ecc81b045
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-08 11:55:01 +0530

    Time: 26 ms (83.92%) | Memory: 42.3 MB (89.38%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,13 +1,14 @@
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        int count = 1;
-        while (count * count <= n) {
-            int sq = count * count;
-            for (int i = sq; i <= n; i++) {
-                dp[i] = Math.min(dp[i - sq] + 1, dp[i]);
+        
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        int count=1;
+        while(count*count<=n){
+            int sq=count*count;
+            for(int i=sq;i<=n;i++){
+                dp[i]=Math.min(dp[i-sq]+1,dp[i]);
             }
             count++;
         }
