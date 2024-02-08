c27bcbdc9f08db36f3514ab9a5d99c4864bcd95d
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-08 11:57:44 +0530

    Time: 24 ms (86.34%) | Memory: 42.6 MB (72.46%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,40 +1,41 @@
-// class Solution {
-//     public int numSquares(int n) {
-        
-//         int dp[]=new int[n+1];
-//         Arrays.fill(dp,Integer.MAX_VALUE);
-//         dp[0]=0;
-//         int count=1;
-//         while(count*count<=n){
-//             int sq=count*count;
-//             for(int i=sq;i<=n;i++){
-//                 dp[i]=Math.min(dp[i-sq]+1,dp[i]);
-//             }
-//             count++;
-//         }
-//         return dp[n];
-//     }
-// }
-
+//dp solution
 class Solution {
     public int numSquares(int n) {
-         int sqrt = (int) Math.sqrt(n);
-
-        if (sqrt * sqrt == n)
-            return 1;
-
-        while (n % 4 == 0)
-            n = n / 4;
-
-        if (n % 8 == 7) return 4;
-
-        for (int i = 1; i * i <= n; i++) {
-            int square = i * i;
-            int base = (int) Math.sqrt(n - square);
-
-            if (base * base == n - square) return 2;
+        
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        int count=1;
+        while(count*count<=n){
+            int sq=count*count;
+            for(int i=sq;i<=n;i++){
+                dp[i]=Math.min(dp[i-sq]+1,dp[i]);
+            }
+            count++;
         }
-
-        return 3;
+        return dp[n];
     }
 }
+
+// class Solution {
+//     public int numSquares(int n) {
+//          int sqrt = (int) Math.sqrt(n);
+
+//         if (sqrt * sqrt == n)
+//             return 1;
+
+//         while (n % 4 == 0)
+//             n = n / 4;
+
+//         if (n % 8 == 7) return 4;
+
+//         for (int i = 1; i * i <= n; i++) {
+//             int square = i * i;
+//             int base = (int) Math.sqrt(n - square);
+
+//             if (base * base == n - square) return 2;
+//         }
+
+//         return 3;
+//     }
+// }
