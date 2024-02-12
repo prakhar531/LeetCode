242d5acc66cdbaa39c24563c258bd22edde901ae
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-12 20:22:07 +0530

    Time: 12 ms (33.06%) | Memory: 48.5 MB (91.64%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // if(nums.length==1){
+        //     return nums[0];
+        // }
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     int count=1;
+        //     while(i<nums.length-1&&nums[i]==nums[i+1]){
+        //         count++;
+        //         i++;
+        //     }
+        //     if(count>nums.length/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Integer key: map.keySet()){
+            if(map.get(key)>nums.length/2 ){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
