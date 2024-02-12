19c939044fae581c36ab9c80ad2a39b932dd00c6
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-12 20:24:56 +0530

    Time: 1 ms (99.82%) | Memory: 50.5 MB (38.59%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,20 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            int count=1;
-            while(i<nums.length-1&&nums[i]==nums[i+1]){
-                count++;
-                i++;
-            }
-            if(count>nums.length/2){
-                return nums[i];
-            }
-        }
-        return -1;
+        // if(nums.length==1){
+        //     return nums[0];
+        // }
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     int count=1;
+        //     while(i<nums.length-1&&nums[i]==nums[i+1]){
+        //         count++;
+        //         i++;
+        //     }
+        //     if(count>nums.length/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
         
         // HashMap<Integer,Integer> map=new HashMap<>();
         // for(int i=0;i<nums.length;i++){
@@ -26,5 +26,17 @@ class Solution {
         //     }
         // }
         // return -1;
+
+        int major=nums[0], count = 1;
+        for(int i=1; i<nums.length;i++){
+            if(count==0){
+                count++;
+                major=nums[i];
+            }else if(major==nums[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
     }
 }
