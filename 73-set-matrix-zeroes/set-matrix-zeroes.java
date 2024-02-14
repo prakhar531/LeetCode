689f8575e57a1453ccc510967cac8913d9618e73
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-14 13:28:29 +0530

    Time: 1 ms (78.11%) | Memory: 45.2 MB (89.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,42 +1,68 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-        ArrayList<Integer> row=new ArrayList<>();
-        ArrayList<Integer> col=new ArrayList<>();
+        // ArrayList<Integer> row=new ArrayList<>();
+        // ArrayList<Integer> col=new ArrayList<>();
+
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(matrix[i][j]==0){
+        //             if(!row.contains(i)){
+        //                 row.add(i);
+        //             }
+        //             if(!col.contains(j)){
+        //                 col.add(j);
+        //             }
+        //         }
+        //     }
+        // }
+        // int itr=0;
+        // if(row.size()==0){
+        //     return;
+        // }
+        // for(int i=0;i<matrix.length;i++){
+        //     if(itr<row.size() && row.get(itr)==i ){
+        //         for(int j=0;j<matrix[0].length;j++)
+        //         {
+        //             matrix[i][j]=0;
+        //         }
+        //         itr++;
+        //     }
+        // }
+        // itr=0;
+        // Collections.sort(col);
+        // for(int i=0;i<matrix[0].length;i++){
+        //     if( itr<col.size()  && col.get(itr)==i){
+        //         for(int j=0;j<matrix.length;j++){
+        //             matrix[j][i]=0;
+        //         }
+        //         itr++;
+        //     }
+        // }
+
+        int row[]=new int[matrix.length];
+        int col[]=new int[matrix[0].length];
 
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    if(!row.contains(i)){
-                        row.add(i);
-                    }
-                    if(!col.contains(j)){
-                        col.add(j);
-                    }
+                    if(row[i]!=1) row[i]=1;
+                    if(col[j]!=1) col[j]=1;
                 }
             }
         }
-        int itr=0;
-        if(row.size()==0){
-            return;
-        }
+
         for(int i=0;i<matrix.length;i++){
-            if(itr<row.size() && row.get(itr)==i ){
-                for(int j=0;j<matrix[0].length;j++)
-                {
-                    matrix[i][j]=0;
-                }
-                itr++;
+            if(row[i]==0) continue;
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=0;
             }
         }
-        itr=0;
-        Collections.sort(col);
+
         for(int i=0;i<matrix[0].length;i++){
-            if( itr<col.size()  && col.get(itr)==i){
-                for(int j=0;j<matrix.length;j++){
-                    matrix[j][i]=0;
-                }
-                itr++;
+            if(col[i]==0) continue;
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][i]=0;
             }
         }
     }
