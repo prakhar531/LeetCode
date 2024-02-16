1d9e04390e9126569f9c534950c9adb6f7c13314
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-16 11:50:34 +0530

    Time: 48 ms (69.11%) | Memory: 63.6 MB (18.11%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,44 +1,68 @@
+// class Solution {
+//     // class Solve implements Comparable<Solve> {
+//     //     int num;
+//     //     int count;
+//     //     public Solve(int num, int count){
+//     //         this.num=num;
+//     //         this.count=count;
+//     //     }
+//     //     @Override
+//     //     public int compareTo(Solve s2){
+//     //         return this.count-s2.count;
+//     //     }
+//     // }
+//     // public int findLeastNumOfUniqueInts(int[] arr, int k) {
+//     //     HashMap<Integer,Integer> map=new HashMap<>();
+//     //     PriorityQueue<Solve> pq=new PriorityQueue<>();
+
+//     //     for(int num:arr) map.put(num, map.getOrDefault(num,0)+1);
+//     //     for(int key:map.keySet()){
+//     //         pq.add(new Solve(key,map.get(key)));
+//     //     }
+
+//     //     while(k>0){
+//     //         Solve curr=pq.poll();
+//     //         if(curr.count==k) break;
+//     //         if(curr.count>1){
+//     //             curr.count--;
+//     //             pq.add(curr);
+//     //         }
+//     //         k--;
+//     //     }
+//     //     return pq.size();
+//     // }
+
+//     public int findLeastNumOfUniqueInts(int[] arr, int k) {
+//         Map<Integer, Integer> count = new HashMap<>();
+//         for (int a : arr)
+//             count.put(a, 1 + count.getOrDefault(a, 0));
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(count.values());
+//         while (k > 0)
+//             k -= pq.poll();
+//         return k < 0 ? pq.size() + 1 : pq.size();        
+//     }
+// }
+
 class Solution {
-    // class Solve implements Comparable<Solve> {
-    //     int num;
-    //     int count;
-    //     public Solve(int num, int count){
-    //         this.num=num;
-    //         this.count=count;
-    //     }
-    //     @Override
-    //     public int compareTo(Solve s2){
-    //         return this.count-s2.count;
-    //     }
-    // }
-    // public int findLeastNumOfUniqueInts(int[] arr, int k) {
-    //     HashMap<Integer,Integer> map=new HashMap<>();
-    //     PriorityQueue<Solve> pq=new PriorityQueue<>();
-
-    //     for(int num:arr) map.put(num, map.getOrDefault(num,0)+1);
-    //     for(int key:map.keySet()){
-    //         pq.add(new Solve(key,map.get(key)));
-    //     }
-
-    //     while(k>0){
-    //         Solve curr=pq.poll();
-    //         if(curr.count==k) break;
-    //         if(curr.count>1){
-    //             curr.count--;
-    //             pq.add(curr);
-    //         }
-    //         k--;
-    //     }
-    //     return pq.size();
-    // }
-
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int a : arr)
-            count.put(a, 1 + count.getOrDefault(a, 0));
-        PriorityQueue<Integer> pq = new PriorityQueue<>(count.values());
-        while (k > 0)
-            k -= pq.poll();
-        return k < 0 ? pq.size() + 1 : pq.size();        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        // List to track all the frequencies
+        List<Integer> frequencies = new ArrayList<>(map.values());
+        Collections.sort(frequencies);
+
+        int elementsRemoved = 0;
+
+        for (int i = 0; i < frequencies.size(); i++) {
+            elementsRemoved += frequencies.get(i);
+            if (elementsRemoved > k) {
+                return frequencies.size() - i;
+            }
+        }
+
+        return 0;
     }
 }
