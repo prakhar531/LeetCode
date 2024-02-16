65ec18d5b3990c42068a6006c937ef69e0e9d4a5
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-16 11:47:17 +0530

    Time: 49 ms (64.26%) | Memory: 63.6 MB (15.39%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,34 +1,44 @@
 class Solution {
-    class Solve implements Comparable<Solve> {
-        int num;
-        int count;
-        public Solve(int num, int count){
-            this.num=num;
-            this.count=count;
-        }
-        @Override
-        public int compareTo(Solve s2){
-            return this.count-s2.count;
-        }
-    }
+    // class Solve implements Comparable<Solve> {
+    //     int num;
+    //     int count;
+    //     public Solve(int num, int count){
+    //         this.num=num;
+    //         this.count=count;
+    //     }
+    //     @Override
+    //     public int compareTo(Solve s2){
+    //         return this.count-s2.count;
+    //     }
+    // }
+    // public int findLeastNumOfUniqueInts(int[] arr, int k) {
+    //     HashMap<Integer,Integer> map=new HashMap<>();
+    //     PriorityQueue<Solve> pq=new PriorityQueue<>();
+
+    //     for(int num:arr) map.put(num, map.getOrDefault(num,0)+1);
+    //     for(int key:map.keySet()){
+    //         pq.add(new Solve(key,map.get(key)));
+    //     }
+
+    //     while(k>0){
+    //         Solve curr=pq.poll();
+    //         if(curr.count==k) break;
+    //         if(curr.count>1){
+    //             curr.count--;
+    //             pq.add(curr);
+    //         }
+    //         k--;
+    //     }
+    //     return pq.size();
+    // }
+
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        PriorityQueue<Solve> pq=new PriorityQueue<>();
-
-        for(int num:arr) map.put(num, map.getOrDefault(num,0)+1);
-        for(int key:map.keySet()){
-            pq.add(new Solve(key,map.get(key)));
-        }
-
-        while(k>0){
-            Solve curr=pq.poll();
-            if(curr.count==k) break;
-            if(curr.count>1){
-                curr.count--;
-                pq.add(curr);
-            }
-            k--;
-        }
-        return pq.size();
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : arr)
+            count.put(a, 1 + count.getOrDefault(a, 0));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(count.values());
+        while (k > 0)
+            k -= pq.poll();
+        return k < 0 ? pq.size() + 1 : pq.size();        
     }
 }
