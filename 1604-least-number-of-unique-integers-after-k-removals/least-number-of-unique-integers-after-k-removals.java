0efe130a815e5b2c6587429f63ea2f3869184a21
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-16 12:09:35 +0530

    Time: 35 ms (94.08%) | Memory: 56.3 MB (94.08%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -43,6 +43,31 @@
 //     }
 // }
 
+// O(nlogn)
+// class Solution {
+//     public int findLeastNumOfUniqueInts(int[] arr, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i : arr) {
+//             map.put(i, map.getOrDefault(i, 0) + 1);
+//         }
+
+//         // List to track all the frequencies
+//         List<Integer> frequencies = new ArrayList<>(map.values());
+//         Collections.sort(frequencies);
+
+//         int elementsRemoved = 0;
+
+//         for (int i = 0; i < frequencies.size(); i++) {
+//             elementsRemoved += frequencies.get(i);
+//             if (elementsRemoved > k) {
+//                 return frequencies.size() - i;
+//             }
+//         }
+
+//         return 0;
+//     }
+// }
+
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -50,19 +75,24 @@ class Solution {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
-        // List to track all the frequencies
-        List<Integer> frequencies = new ArrayList<>(map.values());
-        Collections.sort(frequencies);
+        int n = arr.length;
 
-        int elementsRemoved = 0;
+        int[] countOfFrequencies = new int[n + 1];
 
-        for (int i = 0; i < frequencies.size(); i++) {
-            elementsRemoved += frequencies.get(i);
-            if (elementsRemoved > k) {
-                return frequencies.size() - i;
-            }
+        for (int count : map.values()) {
+            countOfFrequencies[count]++;
         }
 
+        int remainingUniqueElements = map.size();
+        for (int i = 1; i <= n; i++) {
+            int numElementsToRemove = Math.min(k / i, countOfFrequencies[i]);
+
+            k -= (i * numElementsToRemove);
+            remainingUniqueElements -= numElementsToRemove;
+            if (k < i) {
+                return remainingUniqueElements;
+            }
+        }
         return 0;
     }
 }
