536e4885fc63667f7321febc5b41f40c70cb40ab
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-16 11:42:28 +0530

    Time: 53 ms (47.69%) | Memory: 60.8 MB (33.73%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    class Solve implements Comparable<Solve> {
+        int num;
+        int count;
+        public Solve(int num, int count){
+            this.num=num;
+            this.count=count;
+        }
+        @Override
+        public int compareTo(Solve s2){
+            return this.count-s2.count;
+        }
+    }
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        PriorityQueue<Solve> pq=new PriorityQueue<>();
+
+        for(int num:arr) map.put(num, map.getOrDefault(num,0)+1);
+        for(int key:map.keySet()){
+            pq.add(new Solve(key,map.get(key)));
+        }
+
+        while(k>0){
+            Solve curr=pq.poll();
+            if(curr.count>1){
+                curr.count--;
+                pq.add(curr);
+            }
+            k--;
+        }
+
+        return pq.size();
+        
+    }
+}
