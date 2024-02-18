5e27a376933a2b66d134e4100da1fab84f56de45
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-18 22:34:56 +0530

    Time: 53 ms (95.82%) | Memory: 100.7 MB (26.64%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,35 +1,61 @@
 class Solution {
-    public int mostBooked(int n, int[][] meetings) {
-        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
-        PriorityQueue<long[]> queue = new PriorityQueue<>(
-                (a, b) -> a[0] == b[0] ? (int) (a[1] - b[1]) : (int) (a[0] - b[0]));
-        // {endTime, room}
-
-        int[] roomCount = new int[n]; // number of meeting of the room
-        int result = 0;
-
-        for (int i = 0; i < n; i++)
-            queue.add(new long[] { 0, i });
-
-        for (int[] item : meetings) {
-            int time = item[0]; // new meeting start time
-
-            while (queue.peek()[0] < time) // order all available room by room index
-                queue.add(new long[] { time, queue.poll()[1] });
-
-            long[] current = queue.poll();
-            int curRoom = (int) current[1];
-            long meetingEndTime = current[0] + (item[1] - item[0]); // current room endTime + this meeting time
-            roomCount[curRoom]++;
-
-            if (roomCount[curRoom] > roomCount[result]) // update result
-                result = curRoom;
-            else if (roomCount[curRoom] == roomCount[result])
-                result = Math.min(result, curRoom);
-
-            queue.add(new long[] { meetingEndTime, curRoom });
+    public int mostBooked1(int n, int[][] meetings) {
+        Arrays.sort(meetings, (a,b)->a[0]-b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) ->(a[0] == b[0] ? a[1]-b[1] : (a[0]-b[0])));
+        int rooms[] = new int[n];
+        int max = -1, idx = n;
+        for(int i=0;i<n;i++){
+            pq.add(new int[]{0, i});   
         }
-
-        return result;
+        for(int i=0;i<meetings.length;i++){
+            while(!pq.isEmpty() && meetings[i][0]>pq.peek()[0]){
+                pq.add(new int[]{meetings[i][0], pq.poll()[1]});
+            }
+            int arr[] = pq.poll();
+            rooms[arr[1]]++;
+            if(rooms[arr[1]] > max || (rooms[arr[1]] == max && arr[1] < idx)){
+                max = rooms[arr[1]];
+                idx = arr[1];
+            }
+            //System.out.println("max: "+max+" idx: "+idx+" endTime: "+arr[0]);
+            arr[0] = Math.max(arr[0], meetings[i][0]) + meetings[i][1] - meetings[i][0];
+            pq.add(arr);
+        }
+        return idx;
+    }
+    public int mostBooked(int n, int[][] mt) {
+        int[] cnt = new int[n];
+        long[] t = new long[n];
+        Arrays.sort(mt, (a, b)->a[0] - b[0]);
+        for(int[] m : mt){
+            int st = m[0], ed = m[1];
+            boolean f = false;
+            long min = Long.MAX_VALUE;
+            int idx = -1;
+            for(int i = 0; i < n; i++){
+                if(t[i] < min){
+                    min = t[i];
+                    idx = i;
+                }
+                if(t[i] <= st){
+                    t[i] = ed;
+                    f = true;
+                    cnt[i]++;
+                    break;
+                }
+            }
+            if(!f){
+                t[idx] += (ed - st);
+                cnt[idx]++;
+            }
+        }
+        int max = 0, res = 0;
+        for(int i = 0; i < n; i++){
+            if(cnt[i] > max){
+                max = cnt[i];
+                res = i;
+            }
+        }
+        return res;
     }
 }
