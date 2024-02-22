5d5f58bccafc55ed758e42457ac105f5ad0e140f
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-22 15:44:17 +0530

    Time: 3 ms (71.04%) | Memory: 53.8 MB (43.36%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,24 +1,36 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        // if(trust.length==0 && n==1) return 1;
+        // boolean person[]=new boolean[n+1];
+        // for(int i=0;i<trust.length;i++){
+        //    person[trust[i][0]]=true;
+        // }
+
+        // HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        // for(int i=0;i<trust.length;i++){
+        //     if(!map.containsKey(trust[i][1])){
+        //         map.put(trust[i][1],new ArrayList<>());
+        //     }
+        //     map.get(trust[i][1]).add(trust[i][0]);
+        // }
+
+        // for(int i=1;i<person.length;i++){
+        //    if(!person[i] && map.containsKey(i)){
+        //        ArrayList<Integer> temp=map.get(i);
+        //        if(temp.size()==n-1) return i;
+        //    }
+        // }
+        // return -1;
+
         if(trust.length==0 && n==1) return 1;
+        int count[]=new int[n+1];
         boolean person[]=new boolean[n+1];
         for(int i=0;i<trust.length;i++){
-           person[trust[i][0]]=true;
+            count[trust[i][1]]++;
+            person[trust[i][0]]=true;
         }
-
-        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
-        for(int i=0;i<trust.length;i++){
-            if(!map.containsKey(trust[i][1])){
-                map.put(trust[i][1],new ArrayList<>());
-            }
-            map.get(trust[i][1]).add(trust[i][0]);
-        }
-
-        for(int i=1;i<person.length;i++){
-           if(!person[i] && map.containsKey(i)){
-               ArrayList<Integer> temp=map.get(i);
-               if(temp.size()==n-1) return i;
-           }
+        for(int i=1;i<n+1;i++){
+            if(!person[i] && count[i]==n-1) return i;
         }
         return -1;
     }
