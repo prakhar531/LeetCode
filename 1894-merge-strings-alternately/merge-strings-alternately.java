9d4668fc7336622a245d360d0ab665a582961836
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-07 13:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (74.80%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,18 +1,26 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder sb=new StringBuilder("");
+        // StringBuilder sb=new StringBuilder("");
+        // int i=0,j=0;
+        // while(i<word1.length()&&j<word2.length()){
+        //     sb.append(word1.charAt(i++));
+        //     sb.append(word2.charAt(j++));
+        // }
+        // while(i<word1.length()){
+        //     sb.append(word1.charAt(i++));
+        // }
+        // while(j<word2.length()){
+        //     sb.append(word2.charAt(j++));
+        // }
+        // return sb.toString();
 
-        int i=0,j=0;
-        while(i<word1.length()&&j<word2.length()){
-            sb.append(word1.charAt(i++));
-            sb.append(word2.charAt(j++));
+        StringBuilder sb=new StringBuilder("");
+        int minL=Math.min(word1.length(),word2.length());
+        for(int i=0;i<minL;i++){
+            sb.append(word1.charAt(i)).append(word2.charAt(i));
         }
-        while(i<word1.length()){
-            sb.append(word1.charAt(i++));
-        }
-        while(j<word2.length()){
-            sb.append(word2.charAt(j++));
-        }
-        return sb.toString();
+        return sb.append(word1.substring(minL)).append(word2.substring(minL)).toString();
+        
+
     }
 }
