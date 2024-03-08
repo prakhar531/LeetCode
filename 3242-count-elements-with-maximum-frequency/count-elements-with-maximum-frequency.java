03af65cf9c2ba144258fe5556959b23f4f521ec8
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-08 10:57:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (20.81%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -12,18 +12,40 @@ class Solution {
         // }
         // return ans;
 
-        int max=-1;
-        int freq[]=new int[101];
-        for(int it:nums){
-            freq[it]++;
-            if(max<freq[it]) max=freq[it];
+        // int max=-1;
+        // int freq[]=new int[101];
+        // for(int it:nums){
+        //     freq[it]++;
+        //     if(max<freq[it]) max=freq[it];
+        // } 
+        // int ans=0;
+        // for(int i=1;i<101;i++){
+        //     if(max==freq[i]){
+        //         ans+=max;
+        //     }
+        // } 
+        // return ans;
+
+        int maxNum = Integer.MIN_VALUE;
+        for(int num:nums){
+            if(num>maxNum) maxNum = num;
         } 
-        int ans=0;
-        for(int i=1;i<101;i++){
-            if(max==freq[i]){
-                ans+=max;
+       
+        int maxFrequency = 0;
+        int arr[] = new int[maxNum+1];
+        for(int num2:nums){
+           arr[num2]++;
+           if (arr[num2] > maxFrequency) {
+                maxFrequency = arr[num2];
             }
-        } 
-        return ans;
+        }
+
+        int countMaxFrequency = 0;
+        for (int freq : arr) {
+            if (freq == maxFrequency) {
+                countMaxFrequency++;
+            }
+        }
+        return maxFrequency*countMaxFrequency;
     }
 }
