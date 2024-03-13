cd19c8aedd8ff0eecae12c3827524ce158eeb9fa
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-13 23:41:39 +0530

    Time: 1 ms (77.54%) | Memory: 41.2 MB (13.07%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,16 +1,32 @@
+// class Solution {
+//     public int pivotInteger(int n) {
+//         int arr1[]=new int[n];
+//         int arr2[]=new int[n];
+//         arr1[0]=1;
+//         for(int i=1;i<n;i++) arr1[i]=arr1[i-1]+i+1;
+
+//         arr2[n-1]=n;
+        
+//         for(int i=n-2;i>=0;i--) arr2[i]=arr2[i+1]+i+1;
+
+//         for(int i=0;i<n;i++) if(arr1[i]==arr2[i]) return i+1;
+
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int pivotInteger(int n) {
-        int arr1[]=new int[n];
-        int arr2[]=new int[n];
-        arr1[0]=1;
-        for(int i=1;i<n;i++) arr1[i]=arr1[i-1]+i+1;
+        int[] prefs = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            prefs[i] = prefs[i - 1] + i;
+        }
 
-        arr2[n-1]=n;
-        
-        for(int i=n-2;i>=0;i--) arr2[i]=arr2[i+1]+i+1;
+        for (int i = 1; i < n + 1; i++) {
+            if (prefs[i] == prefs[n] - prefs[i - 1])
+                return i;
+        }
 
-        for(int i=0;i<n;i++) if(arr1[i]==arr2[i]) return i+1;
-        
         return -1;
     }
 }
