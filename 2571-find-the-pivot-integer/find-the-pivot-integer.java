22bc7f4358b4a12400ff6d1ebb44375b76dcaea5
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-13 23:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (55.18%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -15,18 +15,27 @@
 //     }
 // }
 
+// class Solution {
+//     public int pivotInteger(int n) {
+//         int[] prefs = new int[n + 1];
+//         for (int i = 1; i < n + 1; i++) {
+//             prefs[i] = prefs[i - 1] + i;
+//         }
+
+//         for (int i = 1; i < n + 1; i++) {
+//             if (prefs[i] == prefs[n] - prefs[i - 1])
+//                 return i;
+//         }
+
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int pivotInteger(int n) {
-        int[] prefs = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            prefs[i] = prefs[i - 1] + i;
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-            if (prefs[i] == prefs[n] - prefs[i - 1])
-                return i;
-        }
-
-        return -1;
+        int y = n*(n+1)/2;
+        int x = (int)Math.sqrt(y);
+        if(x*x==y) return x;
+        else return -1;
     }
 }
