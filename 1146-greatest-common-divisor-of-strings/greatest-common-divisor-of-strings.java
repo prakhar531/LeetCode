097b6bbb17179e5fa7e007439cc164c3af34c0b6
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-15 12:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (47.39%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -6,7 +6,6 @@ class Solution {
         return str1.substring(0,len);        
     }
     public int gcd(int a,int b){
-        if(b==0) return a;
-        return gcd(b,a%b);
+        return b==0?a:gcd(b,a%b);
     }
 }
