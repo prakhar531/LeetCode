e20b37f288c83886ae1476a10c146b6069b2c417
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-17 13:33:30 +0530

    Time: 8 ms (87.70%) | Memory: 46.7 MB (26.56%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (o1,o2)->o1[0]-o2[0]);
+        ArrayList<int[]> ans=new ArrayList<>();
+
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        int i=1;
+        while(i<intervals.length){
+            int s=intervals[i][0];
+            int e=intervals[i][1];
+            if(s<=end){
+                end=Math.max(end,e);
+            }else{
+                ans.add(new int[]{start,end});
+                start=s;
+                end=e;
+            }
+            i++;
+        }
+        ans.add(new int[]{start,end});
+
+        int arr[][]=new int[ans.size()][];
+        for( i=0;i<ans.size();i++){
+            arr[i]=ans.get(i);
+        }
+        return arr;
+    }
+}
