008a2e6b0c7e3e3a36c40ef5a96e92fd382c57d0
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-17 13:30:44 +0530

    Time: 1 ms (98.87%) | Memory: 45 MB (27.76%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {   
-        // ArrayList<int[]> ans = new ArrayList<>();
-        // int idx = 0;
+    //     ArrayList<int[]> ans=new ArrayList<>();
+    //     int idx=0;
+    //     while(idx<intervals.length){
+    //         if(intervals[idx][0]<newInterval[0]){
+    //             ans.add(intervals[idx]);
+    //             idx++;
+    //         }else{
+    //             break;
+    //         }
+    //     }
 
-        // while(idx < intervals.length){
-        //     if(intervals[idx][0] < newInterval[0]){
-        //         ans.add(intervals[idx]);
-        //         idx++;
-        //     }else{
-        //         break;
-        //     }
-        // }
+    //     if(ans.size()==0 || ans.get(ans.size()-1)[1]<newInterval[0]){
+    //         ans.add(newInterval);
+    //     }else{
+    //         ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],newInterval[1]);
+    //     }
 
-        // if(ans.size() == 0 || (newInterval[0] > ans.get(ans.size()-1)[1])){
-        //     ans.add(newInterval);
-        // }else{
-        //     ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1],newInterval[1]);
-        // }
+    //     while(idx<intervals.length){
+    //         int[] curr=ans.get(ans.size()-1);
+    //         if(curr[1]>=intervals[idx][0]){
+    //             curr[1]=Math.max(curr[1],intervals[idx][1]);
+    //         }else{
+    //             ans.add(intervals[idx]);
+    //         }
+    //         idx++;
+    //     }
 
-        // while(idx < intervals.length){
-        //     //reference is getting stored in lastInterval
-        //     int[] lastInterval = ans.get(ans.size()-1);
-        //     if(lastInterval[1] >= intervals[idx][0]){
-        //         lastInterval[1] = Math.max(lastInterval[1], intervals[idx][1]);
-        //     }else{
-        //         ans.add(intervals[idx]);
-        //     }
-        //     idx++;
-        // }
-        // return ans.toArray(new int[ans.size()][]);
+    //     return ans.toArray(new int[ans.size()][]);
+    
 
         ArrayList<int[]> ans=new ArrayList<>();
         int idx=0;
         while(idx<intervals.length){
-            if(intervals[idx][0]<newInterval[0]){
+            if(newInterval[0]>intervals[idx][1]){
                 ans.add(intervals[idx]);
                 idx++;
             }else{
@@ -41,19 +41,15 @@ class Solution {
             }
         }
 
-        if(ans.size()==0 || ans.get(ans.size()-1)[1]<newInterval[0]){
-            ans.add(newInterval);
-        }else{
-            ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],newInterval[1]);
+        while(idx<intervals.length && newInterval[1]>=intervals[idx][0]){
+            newInterval[0]=Math.min(newInterval[0],intervals[idx][0]);
+            newInterval[1]=Math.max(newInterval[1],intervals[idx][1]);
+            idx++;
         }
+        ans.add(newInterval);
 
         while(idx<intervals.length){
-            int[] curr=ans.get(ans.size()-1);
-            if(curr[1]>=intervals[idx][0]){
-                curr[1]=Math.max(curr[1],intervals[idx][1]);
-            }else{
-                ans.add(intervals[idx]);
-            }
+            ans.add(intervals[idx]);
             idx++;
         }
 
