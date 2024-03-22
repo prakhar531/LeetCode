0c82e649f6ec4801d121a54c486b9a604e24088c
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-22 23:29:52 +0530

    Time: 2 ms (100.00%) | Memory: 69.7 MB (6.74%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,44 +9,63 @@
  * }
  */
 class Solution {
-    public ListNode getMid(ListNode head){
-        ListNode slow=head;
-        ListNode fast=head;
+    // public ListNode getMid(ListNode head){
+    //     ListNode slow=head;
+    //     ListNode fast=head;
 
-        while(fast!=null &&  fast.next!=null ){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    public ListNode reverse(ListNode head){
-        ListNode prev=null;
-        ListNode curr=head;
-        ListNode nex=head;
+    //     while(fast!=null &&  fast.next!=null ){
+    //         slow=slow.next;
+    //         fast=fast.next.next;
+    //     }
+    //     return slow;
+    // }
+    // public ListNode reverse(ListNode head){
+    //     ListNode prev=null;
+    //     ListNode curr=head;
+    //     ListNode nex=head;
 
-        while(curr!=null){
-            nex=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nex;
-        }
-        return prev;
-    }
+    //     while(curr!=null){
+    //         nex=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr=nex;
+    //     }
+    //     return prev;
+    // }
+    private static final int[] array = new int[100_000];
     public boolean isPalindrome(ListNode head) {
-        if(head==null) return false;
-        if(head.next==null) return true;
+        // if(head==null) return false;
+        // if(head.next==null) return true;
 
-        ListNode mid=getMid(head);
-        ListNode last=reverse(mid);
+        // ListNode mid=getMid(head);
+        // ListNode last=reverse(mid);
 
-        while(last!=null){
-            if(last.val!=head.val){
-                return false;
-            }
-            last=last.next;
-            head=head.next;
+        // while(last!=null){
+        //     if(last.val!=head.val){
+        //         return false;
+        //     }
+        //     last=last.next;
+        //     head=head.next;
+        // }
+        // return true;
+        if(head==null || head.next==null){
+            return true;
         }
-        return true;
-        
+       ListNode h= head;
+       final int[] array1 = array;
+       int size =0;
+
+       while(h!=null){
+           array1[size++] = h.val;
+           h=h.next;
+       }
+
+       int m = size/2;
+       for(int i=0;i<m;i++){
+           if(array[i] != array1[--size]){
+               return false;
+           }
+       }
+       return true;
     }
 }
