91fa989b76fc9ae87211ae2cd60aa7b52a616170
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-26 19:47:25 +0530

    Time: 1 ms (95.63%) | Memory: 45.4 MB (50.52%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,19 +1,31 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<flowerbed.length;i++){
-            if(flowerbed[i]==1) set.add(i);
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int i=0;i<flowerbed.length;i++){
+        //     if(flowerbed[i]==1) set.add(i);
+        // }
+        // for(int i=0;i<flowerbed.length;i++){
+        //     if(n==0) return true;
+        //     if(flowerbed[i]==0){
+        //         if(!set.contains(i-1) && !set.contains(i+1)){
+        //             n--;
+        //             set.add(i);
+        //         }
+        //     }
+        // }
+        // if(n==0) return true;
+        // return false;
+
+        int k=flowerbed.length;
+        for(int i=0;i<k;i=i+2){
+            if(flowerbed[i] == 0)
+               if(i==flowerbed.length-1 || flowerbed[i]==flowerbed[i+1]){
+                   n--;
+               }
+               else{
+                   i++;
+               }
         }
-        for(int i=0;i<flowerbed.length;i++){
-            if(n==0) return true;
-            if(flowerbed[i]==0){
-                if(!set.contains(i-1) && !set.contains(i+1)){
-                    n--;
-                    set.add(i);
-                }
-            }
-        }
-        if(n==0) return true;
-        return false;
+        return n<=0;
     }
 }
