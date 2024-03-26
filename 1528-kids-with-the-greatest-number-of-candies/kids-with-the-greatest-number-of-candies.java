56bc3c36937550e0124924373d737e0ca05d47e5
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-26 19:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (53.92%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -1,29 +1,46 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int n=candies.length;
-        int left[]=new int[n];
-        int right[]=new int[n];
+        // int n=candies.length;
+        // int left[]=new int[n];
+        // int right[]=new int[n];
 
-        left[0]=candies[0];
-        right[n-1]=candies[n-1];
+        // left[0]=candies[0];
+        // right[n-1]=candies[n-1];
 
-        for(int i=1;i<n;i++){
-            left[i]=Math.max(left[i-1],candies[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            right[i]=Math.max(right[i+1],candies[i]);
-        }
+        // for(int i=1;i<n;i++){
+        //     left[i]=Math.max(left[i-1],candies[i]);
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     right[i]=Math.max(right[i+1],candies[i]);
+        // }
 
-        List<Boolean> ans=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            int curr=candies[i]+extraCandies;
-            if(curr>=left[i]&&curr>=right[i]){
-                ans.add(true);
-            }else{
-                ans.add(false);
+        // List<Boolean> ans=new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     int curr=candies[i]+extraCandies;
+        //     if(curr>=left[i]&&curr>=right[i]){
+        //         ans.add(true);
+        //     }else{
+        //         ans.add(false);
+        //     }
+        // }
+        // return ans;
+
+        List<Boolean> result = new ArrayList<>(candies.length);
+        int maximumCandies = candies[0];
+        for (int kidIndex = 0; kidIndex < candies.length; kidIndex++) {
+            if (maximumCandies < candies[kidIndex]) {
+                maximumCandies = candies[kidIndex];
             }
         }
-        return ans;
+        for (int kidIndex = 0; kidIndex < candies.length; kidIndex++) {
+            if (maximumCandies <= candies[kidIndex] + extraCandies) {
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
         
     }
 }
