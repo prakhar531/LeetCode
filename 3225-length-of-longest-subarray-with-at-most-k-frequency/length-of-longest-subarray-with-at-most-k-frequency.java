e3653d4dfa90c5ded556c8d145e59e1fb9750da2
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-28 12:36:40 +0530

    Time: 28 ms (99.44%) | Memory: 168.2 MB (5.44%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,18 +1,39 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        int max=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
+        // int max=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
 
-        for(int left=0,right=0;right<nums.length;right++){
-            int curr=nums[right];
-            while(left<=right && map.containsKey(curr) && map.get(curr)>=k){
-                int lele=nums[left++];
-                int val=map.get(lele);
-                map.put(lele,val-1);
-            }
-            map.put(curr,map.getOrDefault(curr,0)+1);
-            max=Math.max(max,right-left+1);
+        // for(int left=0,right=0;right<nums.length;right++){
+        //     int curr=nums[right];
+        //     while(left<=right && map.containsKey(curr) && map.get(curr)>=k){
+        //         int lele=nums[left++];
+        //         int val=map.get(lele);
+        //         map.put(lele,val-1);
+        //     }
+        //     map.put(curr,map.getOrDefault(curr,0)+1);
+        //     max=Math.max(max,right-left+1);
+        // }
+        // return max;
+
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+        for(int num:nums){
+            min=Math.min(num,min);
+            max=Math.max(num,max);
         }
-        return max;
+
+        int count[]=new int[max-min+1];
+        int left=0;
+        int ans=0;
+        for(int right=0;right<nums.length;right++){
+            count[nums[right]-min]++;
+            while(count[nums[right]-min]>k){
+                count[nums[left]-min]--;
+                left++;
+            }
+            ans=Math.max(ans,right-left+1);
+        }
+        return ans;
     }
 }
