cb36105a5cdb842a06d8c80c3cf31024c61b9643
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-29 10:10:03 +0530

    Time: 5 ms (84.18%) | Memory: 65.5 MB (41.33%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,32 +1,32 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        // int max=Integer.MIN_VALUE;
-        // for(int num:nums) max=Math.max(num,max);
-        // long ans=0;
-        // int count=0;
-
-        // for(int left=0,right=0;right<nums.length;right++){
-        //     if(nums[right]==max) count++;
-        //     while(count==k){
-        //         if(nums[left]==max) count--;
-        //         left++;
-        //     }
-        //     ans+=left;
-        // }
-        // return ans;
-        
         int max=Integer.MIN_VALUE;
         for(int num:nums) max=Math.max(num,max);
-        List<Integer> list=new ArrayList<>();
         long ans=0;
+        int count=0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==max) list.add(i);
-            int freq=list.size();
-            if(freq>=k){
-                ans+=list.get(freq-k)+1;
+        for(int left=0,right=0;right<nums.length;right++){
+            if(nums[right]==max) count++;
+            while(count==k){
+                if(nums[left]==max) count--;
+                left++;
             }
+            ans+=left;
         }
         return ans;
+        
+        // int max=Integer.MIN_VALUE;
+        // for(int num:nums) max=Math.max(num,max);
+        // List<Integer> list=new ArrayList<>();
+        // long ans=0;
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==max) list.add(i);
+        //     int freq=list.size();
+        //     if(freq>=k){
+        //         ans+=list.get(freq-k)+1;
+        //     }
+        // }
+        // return ans;
     }
 }
