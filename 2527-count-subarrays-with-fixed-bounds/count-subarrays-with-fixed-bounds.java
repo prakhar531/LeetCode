91c23c3b642f38ff84863746e0b41a6ca44fd3bb
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-31 12:20:00 +0530

    Time: 5 ms (100.00%) | Memory: 59.6 MB (59.57%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
@@ -1,20 +1,38 @@
+// class Solution {
+//     public long countSubarrays(int[] nums, int minK, int maxK) {
+//         long res=0;
+//         int badIdx=-1, leftIdx=-1, rightIdx=-1;
+
+//         for(int i=0;i<nums.length;i++){
+//             if(!(minK<=nums[i] && nums[i]<=maxK)){
+//                 badIdx=i;
+//             }
+//             if(nums[i]==minK){
+//                 leftIdx=i;
+//             }
+//             if(nums[i]==maxK){
+//                 rightIdx=i;
+//             }
+//             res+=Math.max(0,Math.min(leftIdx,rightIdx)-badIdx);
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        long res=0;
-        int badIdx=-1, leftIdx=-1, rightIdx=-1;
-
-        for(int i=0;i<nums.length;i++){
-            if(!(minK<=nums[i] && nums[i]<=maxK)){
-                badIdx=i;
+        int minI = -1, maxI = -1, left = -1, right = 0;
+        long count = 0;
+        while(right < nums.length){
+            if(nums[right] < minK || nums[right] > maxK){
+                minI = right;
+                maxI = right;
+                left = right;
             }
-            if(nums[i]==minK){
-                leftIdx=i;
-            }
-            if(nums[i]==maxK){
-                rightIdx=i;
-            }
-            res+=Math.max(0,Math.min(leftIdx,rightIdx)-badIdx);
+            minI = nums[right] == minK ? right : minI;
+            maxI = nums[right] == maxK ?  right : maxI;
+            count += Math.min(minI, maxI) - left;
+            right++;
         }
-        return res;
+        return count;
     }
 }
