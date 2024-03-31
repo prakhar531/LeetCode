d5a7306f9aba7789271b3353cd63da718147ff34
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-03-31 13:57:06 +0530

    Time: 9 ms (100.00%) | Memory: 59.4 MB (50.00%) - LeetSync

--- 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.java
+++ 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.java
@@ -1,19 +1,31 @@
 class Solution {
     public int minimumLevels(int[] possible) {
-        int n=possible.length;
-        for(int i=0;i<n;i++) if(possible[i]==0) possible[i]=-1;
-        int prefix[]=new int[n];
+        // int n=possible.length;
+        // for(int i=0;i<n;i++) if(possible[i]==0) possible[i]=-1;
+        // int prefix[]=new int[n];
 
-        prefix[0]=possible[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+possible[i];
+        // prefix[0]=possible[0];
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=prefix[i-1]+possible[i];
+        // }
+        // for(int i=0;i<n-1;i++){
+        //     if(prefix[n-1]-prefix[i]<prefix[i]){
+        //         return i+1;  
+        //     } 
+        // }
+        // return -1;   
+
+        int sum=0;
+        for(int num:possible){
+            if(num==0) sum--;
+            else sum++;
         }
-        for(int i=0;i<n-1;i++){
-            if(prefix[n-1]-prefix[i]<prefix[i]){
-                return i+1;  
-            } 
+        int daniel=0;
+        for(int i=0;i<possible.length-1;i++){
+            if(possible[i]==0) daniel--;
+            else daniel++;
+            if(daniel>sum-daniel) return i+1;
         }
         return -1;
-        
     }
 }
