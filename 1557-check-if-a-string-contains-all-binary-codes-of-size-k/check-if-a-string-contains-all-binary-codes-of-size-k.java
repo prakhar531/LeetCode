40d0d93dac9cb66d43cf21fdc9827df1af5851d6
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-02 21:39:00 +0530

    Time: 9 ms (99.75%) | Memory: 46 MB (92.39%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -12,14 +12,32 @@ class Solution {
         // if(set.size()==Math.pow(2,k)) return true;
         // return false;
 
-        HashSet<String> set=new HashSet<>();
-        int temp=(int)Math.pow(2,k);
-        for(int i=k-1;i<s.length();i++){
-            String str=s.substring(i-k+1,i+1);
-            set.add(str);
-            if(set.size()==temp) return true;
-        }
+        // HashSet<String> set=new HashSet<>();
+        // int temp=(int)Math.pow(2,k);
+        // for(int i=k-1;i<s.length();i++){
+        //     String str=s.substring(i-k+1,i+1);
+        //     set.add(str);
+        //     if(set.size()==temp) return true;
+        // }
         
+        // return false;
+
+        int size = 1 << k;
+        boolean[] found = new boolean[size];
+        int allOne = size-1;
+        int hash = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            hash = (hash << 1) & allOne | (s.charAt(i) - '0');
+            if (i+1 >= k && !found[hash]) {
+                found[hash] = true;
+                size--;
+                if (size == 0) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
