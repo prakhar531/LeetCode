43a6907b2f350a8d0a2565d30ce0f012b51917eb
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-02 21:12:53 +0530

    Time: 3 ms (93.57%) | Memory: 60.9 MB (37.24%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];
             if(i>=k-1){
-                if(sum/k>=threshold) ans++;
+                if(sum>=threshold*k) ans++;
                 sum-=arr[i-k+1];
             }
         }
