178e2814583d453ba58e5fe5215774f1b14188e8
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-02 12:30:17 +0530

    Time: 9 ms (68.74%) | Memory: 42.6 MB (19.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,43 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        if(s == null || s.length() <= 1) return true;
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        char sArr[]=s.toCharArray();
+        char tArr[]=t.toCharArray();
 
 
-        HashMap<Character,Character> map1=new HashMap<>();
-        map1.put(s.charAt(0),t.charAt(0));
-        HashMap<Character,Character> map2=new HashMap<>();
-        map2.put(t.charAt(0),s.charAt(0));
-        for(int i=1;i<s.length();i++){
-            if(map1.containsKey(s.charAt(i)) && map1.get(s.charAt(i))!=t.charAt(i)){
-                return false;
+        for(int i = 0 ; i<sArr.length; i++){
+            char a = sArr[i];
+            char b = tArr[i];
+            if(map.containsKey(a)){
+                 if(map.get(a).equals(b))
+                    continue;
+                else
+                    return false;
             }else{
-                map1.put(s.charAt(i),t.charAt(i));
-            }
-            if(map2.containsKey(t.charAt(i)) && map2.get(t.charAt(i))!=s.charAt(i)){
-                return false;
-            }else{
-                map2.put(t.charAt(i),s.charAt(i));
+                if(!map.containsValue(b))
+                    map.put(a,b);
+                else 
+                    return false;
+                
             }
         }
         return true;
-        // if(s == null || s.length() <= 1) return true;
-        // HashMap<Character, Character> map = new HashMap<Character, Character>();
-        // for(int i = 0 ; i< s.length(); i++){
-        //     char a = s.charAt(i);
-        //     char b = t.charAt(i);
-        //     if(map.containsKey(a)){
-        //          if(map.get(a).equals(b))
-        //             continue;
-        //         else
-        //             return false;
-        //     }else{
-        //         if(!map.containsValue(b))
-        //             map.put(a,b);
-        //         else 
-        //             return false;
-                
-        //     }
-        // }
-        // return true;
+        
         
     
     }
