e4b44055f41aaeeb00858be2e64c5f191a903f60
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-03 11:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (58.61%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,20 +1,32 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        if(s.length()<3) return 0;
+        // if(s.length()<3) return 0;
 
-        HashMap<Character,Integer> map=new HashMap<>();
-        int ans=0;
-        char charr[]=s.toCharArray();
-        for(int i=0;i<charr.length;i++){
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // int ans=0;
+        // char charr[]=s.toCharArray();
+        // for(int i=0;i<charr.length;i++){
 
-            map.put(charr[i],map.getOrDefault(charr[i],0)+1);
-            if(i>=2){
-                if(map.size()==3) ans++;
+        //     map.put(charr[i],map.getOrDefault(charr[i],0)+1);
+        //     if(i>=2){
+        //         if(map.size()==3) ans++;
 
-                if(map.get(charr[i-2])==1) map.remove(charr[i-2]);
-                else map.put(charr[i-2],map.get(charr[i-2])-1);
-            }
+        //         if(map.get(charr[i-2])==1) map.remove(charr[i-2]);
+        //         else map.put(charr[i-2],map.get(charr[i-2])-1);
+        //     }
+        // }
+        // return ans;
+
+        int res = 0;
+        var chars = s.toCharArray();
+        int i = 0;
+        while (i <= chars.length - 3){
+            char uno = chars[i];
+            char duo = chars[i + 1];
+            char tre = chars[i + 2];
+            if (uno != duo && duo != tre && uno != tre) res++;
+            i++;
         }
-        return ans;
+        return res;
     }
 }
