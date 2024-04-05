811b062c8325d7dc0d2622aae87b944c0a4246c7
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-05 17:17:20 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (57.36%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,28 +1,62 @@
-class Solution {
-    public String makeGood(String str) {
-        Stack<Character> s=new Stack<>();
+// class Solution {
+//     public String makeGood(String str) {
+//         if (s.length() == 0 || s.length() == 1)
+//             return s;
+//         Stack<Character> s=new Stack<>();
 
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(s.isEmpty()){
-                s.push(ch);
-                continue;
-            }
-            if(ch==s.peek()){
-                s.push(ch);
-                continue;
-            }
-            if(Character.toUpperCase(ch)==s.peek() || Character.toLowerCase(ch)==s.peek()){
-                s.pop();
+//         for(int i=0;i<str.length();i++){
+//             char ch=str.charAt(i);
+//             if(s.isEmpty()){
+//                 s.push(ch);
+//                 continue;
+//             }
+//             if(ch==s.peek()){
+//                 s.push(ch);
+//                 continue;
+//             }
+//             if(Character.toUpperCase(ch)==s.peek() || Character.toLowerCase(ch)==s.peek()){
+//                 s.pop();
                
-            }else{
-                s.push(ch);
+//             }else{
+//                 s.push(ch);
+//             }
+//         }
+//         StringBuilder sb=new StringBuilder("");
+//         while(!s.isEmpty()){
+//             sb.append(s.pop());
+//         } 
+//         return sb.reverse().toString();
+//     }
+// }
+
+class Solution {
+    public String makeGood(String s) {
+        if (s.length() == 0 || s.length() == 1)
+            return s;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(s);
+
+        int i = 0;
+
+        while (i < sb.length() - 1)
+        {
+            char leftChar = sb.charAt(i);
+            char rightChar = sb.charAt(i + 1);
+
+            if (Math.abs(leftChar - rightChar) == 32)
+            {
+                sb.delete(i, i + 2);
+                if (i > 0)
+                    i--;
             }
+            else
+            {
+                i++;
+            } 
         }
-        StringBuilder sb=new StringBuilder("");
-        while(!s.isEmpty()){
-            sb.append(s.pop());
-        } 
-        return sb.reverse().toString();
+
+        return sb.toString();
     }
 }
