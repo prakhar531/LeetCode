b66b0165eb3d0c13c02ff04987e374967dc6f6a5
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-17 18:50:04 +0530

    Time: 1 ms (99.37%) | Memory: 44.1 MB (76.94%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -13,21 +13,42 @@
  *     }
  * }
  */
+// class Solution {
+//     String small="";
+//     public String smallestFromLeaf(TreeNode root) {
+//         solve(root,"");
+//         return small;
+//     }
+//     public void solve(TreeNode root,String curr){
+//         if(root==null) return;
+//         curr=(char)(root.val+'a')+curr;
+//         if(root.left==null&&root.right==null){
+//             if(small=="" || small.compareTo(curr)>0){
+//                 small=curr;
+//             }
+//         }
+//         if(root.right!=null) solve(root.right,curr);
+//         if(root.left!=null) solve(root.left,curr);
+//     }
+// }
+
 class Solution {
     String small="";
     public String smallestFromLeaf(TreeNode root) {
-        solve(root,"");
+        solve(root,new StringBuilder());
         return small;
     }
-    public void solve(TreeNode root,String curr){
+    public void solve(TreeNode root,StringBuilder sb){
         if(root==null) return;
-        curr=(char)(root.val+'a')+curr;
+        sb.insert(0,(char)(root.val+'a'));
+
         if(root.left==null&&root.right==null){
-            if(small=="" || small.compareTo(curr)>0){
-                small=curr;
+            if(small=="" || small.compareTo(sb.toString())>0){
+                small=sb.toString();
             }
         }
-        if(root.right!=null) solve(root.right,curr);
-        if(root.left!=null) solve(root.left,curr);
+        if(root.right!=null) solve(root.right,sb);
+        if(root.left!=null) solve(root.left,sb);
+        sb.deleteCharAt(0);
     }
 }
