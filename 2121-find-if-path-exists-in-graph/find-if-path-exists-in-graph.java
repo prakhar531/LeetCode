8b5e14a214ef75af633fdf416ed4125732e1be43
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-21 23:57:49 +0530

    Time: 119 ms (47.26%) | Memory: 144.4 MB (57.87%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,64 @@
+class Solution {
+    public class Edge{
+        int src;
+        int dest;
+        public Edge(int src,int dest){
+            this.src=src;
+            this.dest=dest;
+        }
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean vis[]=new boolean[n];
+        ArrayList<Edge> graph[]=new ArrayList[n];
+        for(int i=0;i<n;i++) graph[i]=new ArrayList<>();
+        
+        for(int i=0;i<edges.length;i++){
+            graph[edges[i][0]].add(new Edge(edges[i][0],edges[i][1]));
+            graph[edges[i][1]].add(new Edge(edges[i][1],edges[i][0]));
+        }
+        return dfs(graph,vis,source,destination);
+    }
+    public boolean dfs(ArrayList<Edge> graph[],boolean vis[],int src,int dest){
+        if(src==dest) return true;
+        vis[src]=true;
+        for(int i=0;i<graph[src].size();i++){
+            Edge e=graph[src].get(i);
+            if((!vis[e.dest]) && dfs(graph,vis,e.dest,dest)) return true;
+        }
+        return false;
+    }
+}
+/*
+TC: O(V+E)
+SC: O(V+E)
+*/
+// class Solution {
+//     boolean found = false;
+//     public boolean validPath(int n, int[][] edges, int start, int end) {
+//         if(start == end) return  true;
+        
+//         Map<Integer,List<Integer>> graph = new HashMap();
+//         boolean[] visited = new boolean[n];
+        
+//         for(int i = 0 ; i < n ; i++) graph.put(i, new ArrayList());
+//         for(int[] edge : edges){
+//            graph.get(edge[0]).add(edge[1]);
+//            graph.get(edge[1]).add(edge[0]);
+//         }
+//         dfs(graph,visited,start,end);
+//         return found;
+//     }
+    
+//     private void dfs(Map<Integer,List<Integer>> graph,boolean[] visited, int start, int end){
+//         if(visited[start] || found) return;
+//         visited[start] = true;
+//         for(int nei : graph.get(start)){
+//             if(nei == end){
+//                 found = true;
+//                 break;
+//             }
+//             if(!visited[nei])
+//                 dfs(graph, visited, nei, end);
+//         }
+//     }
+// }
