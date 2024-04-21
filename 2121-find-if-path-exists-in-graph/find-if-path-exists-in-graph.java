c51bb58db8ee6c08d711b6400fbe0c2d9858a01a
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-22 00:00:38 +0530

    Time: 3 ms (99.62%) | Memory: 109.1 MB (95.43%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,31 +1,51 @@
+// class Solution {
+//     public class Edge{
+//         int src;
+//         int dest;
+//         public Edge(int src,int dest){
+//             this.src=src;
+//             this.dest=dest;
+//         }
+//     }
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         boolean vis[]=new boolean[n];
+//         ArrayList<Edge> graph[]=new ArrayList[n];
+//         for(int i=0;i<n;i++) graph[i]=new ArrayList<>();
+
+//         for(int i=0;i<edges.length;i++){
+//             graph[edges[i][0]].add(new Edge(edges[i][0],edges[i][1]));
+//             graph[edges[i][1]].add(new Edge(edges[i][1],edges[i][0]));
+//         }
+//         return dfs(graph,vis,source,destination);
+//     }
+//     public boolean dfs(ArrayList<Edge> graph[],boolean vis[],int src,int dest){
+//         if(src==dest) return true;
+//         vis[src]=true;
+//         for(int i=0;i<graph[src].size();i++){
+//             Edge e=graph[src].get(i);
+//             if((!vis[e.dest]) && dfs(graph,vis,e.dest,dest)) return true;
+//         }
+//         return false;
+//     }
+// }
 class Solution {
-    public class Edge{
-        int src;
-        int dest;
-        public Edge(int src,int dest){
-            this.src=src;
-            this.dest=dest;
-        }
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        boolean vis[]=new boolean[n];
-        ArrayList<Edge> graph[]=new ArrayList[n];
-        for(int i=0;i<n;i++) graph[i]=new ArrayList<>();
-        
-        for(int i=0;i<edges.length;i++){
-            graph[edges[i][0]].add(new Edge(edges[i][0],edges[i][1]));
-            graph[edges[i][1]].add(new Edge(edges[i][1],edges[i][0]));
+        if(edges.length == 0) return true;
+        boolean[] visited = new boolean[n];
+        boolean flag = true;
+        visited[source] = true;
+    while(flag){
+        flag = false;
+        for(int[] edge : edges){
+            if(visited[edge[0]] != visited[edge[1]]){
+                visited[edge[0]] = true;
+                visited[edge[1]] = true;
+                flag = true;
+            }
+            if(visited[destination]) return true;
         }
-        return dfs(graph,vis,source,destination);
     }
-    public boolean dfs(ArrayList<Edge> graph[],boolean vis[],int src,int dest){
-        if(src==dest) return true;
-        vis[src]=true;
-        for(int i=0;i<graph[src].size();i++){
-            Edge e=graph[src].get(i);
-            if((!vis[e.dest]) && dfs(graph,vis,e.dest,dest)) return true;
-        }
-        return false;
+    return false;
     }
 }
 /*
