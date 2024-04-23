1760b9ecb5551f868cfdd45a0d60bdcad96a5918
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-04-23 12:14:26 +0530

    Time: 9 ms (99.53%) | Memory: 55.4 MB (84.12%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,30 +1,73 @@
+// class Solution {
+//     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+//         if(n==1) return Collections.singletonList(0);
+
+//         List<Set<Integer>> adj=new ArrayList<>();
+//         for(int i=0;i<n;i++) adj.add(new HashSet<>());
+
+//         for(int[] temp:edges){
+//             adj.get(temp[0]).add(temp[1]);
+//             adj.get(temp[1]).add(temp[0]);
+//         }
+
+//         List<Integer> leaves=new ArrayList<>();
+//         for(int i=0;i<n;i++){
+//             if(adj.get(i).size()==1) leaves.add(i);
+//         }
+
+//         while(n>2){
+//             n-=leaves.size();
+//             List<Integer> newleaves=new ArrayList<>();
+//             for(int i:leaves){
+//                 int j=adj.get(i).iterator().next();
+//                 adj.get(j).remove(i);
+//                 if(adj.get(j).size()==1) newleaves.add(j);
+//             }
+//             leaves=newleaves;
+//         }
+//         return leaves;
+//     }
+// }
+
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n==1) return Collections.singletonList(0);
-
-        List<Set<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new HashSet<>());
-
-        for(int[] temp:edges){
-            adj.get(temp[0]).add(temp[1]);
-            adj.get(temp[1]).add(temp[0]);
+        int[] counts = new int[n];
+        int[] links = new int[n];
+        for (int[] edge : edges) {
+            links[edge[0]] ^= edge[1];
+            counts[edge[0]]++;
+            links[edge[1]] ^= edge[0];
+            counts[edge[1]]++;
         }
-
-        List<Integer> leaves=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(adj.get(i).size()==1) leaves.add(i);
+        Queue<Integer> Qu = new LinkedList<>();
+        int[] dists = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (counts[i] == 1)
+                Qu.add(i);
         }
-
-        while(n>2){
-            n-=leaves.size();
-            List<Integer> newleaves=new ArrayList<>();
-            for(int i:leaves){
-                int j=adj.get(i).iterator().next();
-                adj.get(j).remove(i);
-                if(adj.get(j).size()==1) newleaves.add(j);
+        int stp = 1;
+        while (!Qu.isEmpty()) {
+            int size = Qu.size();
+            for (int j = 0; j < size; j++) {
+                int tmp = Qu.poll();
+                links[links[tmp]] ^= tmp;
+                counts[links[tmp]]--;
+                if (counts[links[tmp]] == 1) {
+                    dists[links[tmp]] = Math.max(stp, dists[links[tmp]]);
+                    Qu.add(links[tmp]);
+                }
             }
-            leaves=newleaves;
+            stp++;
         }
-        return leaves;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(dists[i], max);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (dists[i] == max)
+                res.add(i);
+        }
+        return res;
     }
 }
