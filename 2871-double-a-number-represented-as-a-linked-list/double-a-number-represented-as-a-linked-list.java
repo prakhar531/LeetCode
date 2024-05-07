5e7aa7a9b7fd792e421d09569c24af438ea9b3a8
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-07 11:20:55 +0530

    Time: 21 ms (15.35%) | Memory: 45.8 MB (74.11%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -9,31 +9,50 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        ListNode prev=null,curr=head,nex;
-        while(curr!=null){
-            nex=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nex;
-        }
-        return prev;
-    }
+    // public ListNode reverse(ListNode head){
+    //     ListNode prev=null,curr=head,nex;
+    //     while(curr!=null){
+    //         nex=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr=nex;
+    //     }
+    //     return prev;
+    // }
+    // public ListNode doubleIt(ListNode head) {
+    //     head=reverse(head);
+    //     ListNode prev=null,curr=head;
+    //     int carry=0;
+    //     while(curr!=null){
+    //         int newval=curr.val*2;
+    //         curr.val=(newval%10)+carry;
+    //         carry=newval>9?1:0;
+    //         prev=curr;
+    //         curr=curr.next;
+    //     }
+    //     if(carry!=0){
+    //         ListNode newNode=new ListNode(carry);
+    //         prev.next=newNode;
+    //     }
+    //     return reverse(head);
+    // }
+
     public ListNode doubleIt(ListNode head) {
-        head=reverse(head);
-        ListNode prev=null,curr=head;
-        int carry=0;
-        while(curr!=null){
-            int newval=curr.val*2;
-            curr.val=(newval%10)+carry;
-            carry=newval>9?1:0;
-            prev=curr;
-            curr=curr.next;
+        Stack<Integer> s=new Stack<>();
+        int nums=0;
+
+        while(head!=null){
+            s.push(head.val);
+            head=head.next;
         }
-        if(carry!=0){
-            ListNode newNode=new ListNode(carry);
-            prev.next=newNode;
+
+        ListNode tail=null;
+        while(!s.isEmpty()||nums!=0){
+            tail=new ListNode(0,tail);
+            if(!s.isEmpty()) nums+=s.pop()*2;
+            tail.val=nums%10;
+            nums/=10;
         }
-        return reverse(head);
+        return tail;
     }
 }
