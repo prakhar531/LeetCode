f6ffc106aeaa79ee574daa65e8ef72d6f8dfd48e
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-08 23:23:01 +0530

    Time: 2 ms (100.00%) | Memory: 44.9 MB (99.48%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -39,36 +39,78 @@
 //     }
 // }
 
+// class Solution {
+//     public String[] findRelativeRanks(int[] score) {
+        
+//         int n = score.length;
+//         String[] res = new String[n];
+        
+//         PriorityQueue<Integer> pq = 
+//             new PriorityQueue<>((a,b)->score[b]-score[a]);
+        
+//         for(int i=0;i<n;i++){
+//             pq.add(i);
+//         }
+//         int i=1;
+//         while(!pq.isEmpty()){
+            
+//             int idx = pq.poll();
+            
+//             if(i>3){
+//                 res[idx] = Integer.toString(i);
+//             }else if(i==1){
+//                 res[idx] = "Gold Medal";
+//             }else if(i==2){
+//                 res[idx] = "Silver Medal";
+//             }else if(i==3){
+//                 res[idx] = "Bronze Medal";
+//             }
+//             i++;
+//         }
+        
+//         return res;
+        
+//     }
+// }
+
 class Solution {
-    public String[] findRelativeRanks(int[] score) {
-        
-        int n = score.length;
-        String[] res = new String[n];
-        
-        PriorityQueue<Integer> pq = 
-            new PriorityQueue<>((a,b)->score[b]-score[a]);
-        
-        for(int i=0;i<n;i++){
-            pq.add(i);
-        }
-        int i=1;
-        while(!pq.isEmpty()){
-            
-            int idx = pq.poll();
-            
-            if(i>3){
-                res[idx] = Integer.toString(i);
-            }else if(i==1){
-                res[idx] = "Gold Medal";
-            }else if(i==2){
-                res[idx] = "Silver Medal";
-            }else if(i==3){
-                res[idx] = "Bronze Medal";
+    private int findMax(int[] score) {
+        int maxScore = 0;
+        for (int s : score) {
+            if (s > maxScore) {
+                maxScore = s;
             }
-            i++;
         }
-        
-        return res;
-        
+        return maxScore;
+    }
+
+    public String[] findRelativeRanks(int[] score) {
+        int N = score.length;
+
+        // Add the original index of each score to the array
+        // Where the score is the key
+        int M = findMax(score);
+        int[] scoreToIndex = new int[M + 1];
+        for (int i = 0; i < N; i++) {
+            scoreToIndex[score[i]] = i + 1;
+        }
+
+        final String[] MEDALS = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+
+        // Assign ranks to athletes
+        String[] rank = new String[N];
+        int place = 1;
+        for (int i = M; i >= 0; i--) {
+            if (scoreToIndex[i] != 0) {
+                int originalIndex = scoreToIndex[i] - 1;
+                if (place < 4) {
+                    rank[originalIndex] = MEDALS[place - 1];
+                } else {
+                    rank[originalIndex] = String.valueOf(place);
+                }
+                place++;
+            }
+        }
+        return rank;
     }
 }
