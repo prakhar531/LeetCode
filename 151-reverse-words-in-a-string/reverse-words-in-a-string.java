e44a24549df5c0df426b5222a5f18b5b9110587d
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-10 18:09:34 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (98.14%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,38 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] arr=s.split("\\s+");
+//         StringBuilder sb=new StringBuilder("");
+//         int n=arr.length;
+//         for(int i=n-1;i>=0;i--){
+//             sb.append(arr[i]);
+//             sb.append(" ");
+//         }
+//         return sb.toString().trim();
+//     }
+// }
+
 class Solution {
     public String reverseWords(String s) {
-        String[] arr=s.split("\\s+");
-        StringBuilder sb=new StringBuilder("");
-        int n=arr.length;
-        for(int i=n-1;i>=0;i--){
-            sb.append(arr[i]);
-            sb.append(" ");
+        char[] str = s.toCharArray();
+        char[] res = new char[s.length() + 1];
+        int i = s.length() - 1;
+        int j;
+        int k = 0;
+         while(i >= 0) {
+            while(i >= 0 && str[i] == ' ') {
+                i--;
+            }
+
+            if(i < 0) break;
+            j = i;
+            while(i >= 0 && str[i]!=' ') {
+                i--;
+            }
+            for(int l = i+1;l <= j;l++) {
+                res[k++] = str[l];
+            }
+            res[k++] = ' ';
         }
-        return sb.toString().trim();
+        return new String(res, 0, k - 1);
     }
 }
