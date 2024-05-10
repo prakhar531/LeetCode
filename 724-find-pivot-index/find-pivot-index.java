35422b325d0585eb4ef4f9a722e2e839453651b1
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-10 18:58:53 +0530

    Time: 1 ms (96.05%) | Memory: 45.5 MB (34.55%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -6,16 +6,16 @@ class Solution {
         lsum[0]=nums[0];
         rsum[n-1]=nums[n-1];
 
-        for(int i=1;i<n;i++){
-            lsum[i]=lsum[i-1]+nums[i];
-        }
         for(int i=n-2;i>=0;i--){
             rsum[i]=rsum[i+1]+nums[i];
         }
-
-        for(int i=0;i<n;i++){
+        if(lsum[0]==rsum[0]) return 0;
+        for(int i=1;i<n;i++){
+            lsum[i]=lsum[i-1]+nums[i];
             if(lsum[i]==rsum[i]) return i;
         }
+        
+       
         return -1;
     }
 }
