853437858e2b0ca47993c7578d354f66c109ebdd
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-12 02:12:59 +0530

    Time: 24 ms (99.21%) | Memory: 45.8 MB (48.81%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
@@ -1,16 +1,30 @@
+class Worker implements Comparable<Worker> {
+    final int q, w;
+    public Worker(int q, int w) {
+        this.q = q;
+        this.w = w;
+    }
+    @Override
+    public int compareTo(Worker other) {
+        return Integer.compare(w * other.q, q * other.w);
+    }
+}
 class Solution {
-        public double mincostToHireWorkers(int[] q, int[] w, int K) {
-        double[][] workers = new double[q.length][2];
-        for (int i = 0; i < q.length; ++i)
-            workers[i] = new double[]{(double)(w[i]) / q[i], (double)q[i]};
-        Arrays.sort(workers, (a, b) -> Double.compare(a[0], b[0]));
-        double res = Double.MAX_VALUE, qsum = 0;
-        PriorityQueue<Double> pq = new PriorityQueue<>();
-        for (double[] worker: workers) {
-            qsum += worker[1];
-            pq.add(-worker[1]);
-            if (pq.size() > K) qsum += pq.poll();
-            if (pq.size() == K) res = Math.min(res, qsum * worker[0]);
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        int n = quality.length;
+        Worker[] a = new Worker[n];
+        for (int i = 0; i < n; ++i) {
+            a[i] = new Worker(quality[i], wage[i]);
+        }
+        Arrays.sort(a);
+        int s = 0;
+        double res = 1e15;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (Worker worker: a) {
+            q.add(-worker.q);
+            s += worker.q;
+            if (q.size() > k) s += q.poll();
+            if (q.size() == k) res = Math.min(res, (double) s * worker.w / worker.q);
         }
         return res;
     }
