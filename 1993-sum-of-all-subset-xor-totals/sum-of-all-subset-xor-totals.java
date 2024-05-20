162c2112633e5b682fe4bf058ba81a15bef0214e
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-20 22:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (96.23%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,35 +1,15 @@
 class Solution {
-    public int subsetXORSum(int[] nums) {
-        List<List<Integer>> subsets = new ArrayList<>();
-        // Generate all of the subsets
-        generateSubsets(nums, 0, new ArrayList<>(), subsets);
-
-        // Compute the XOR total for each subset and add to the result
-        int result = 0;
-        for (List<Integer> subset : subsets) {
-            int subsetXORTotal = 0;
-            for (int num : subset) {
-                subsetXORTotal ^= num;
-            }
-            result += subsetXORTotal;
-        }
-        return result;
-    }
-
-    private void generateSubsets(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets) {
-        // Base case: index reached end of nums
-        // Add the current subset to subsets
-        if (index == nums.length) {
-            subsets.add(new ArrayList<>(subset));
+    int ans=0;
+    public void solve(int[] nums,int i,int xor){
+        if(i>=nums.length){
+            ans+=xor;
             return;
         }
-
-        // Generate subsets with nums[i]
-        subset.add(nums[index]);
-        generateSubsets(nums, index + 1, subset, subsets);
-        subset.remove(subset.size() - 1);
-
-        // Generate subsets without nums[i]
-        generateSubsets(nums, index + 1, subset, subsets);
+        solve(nums,i+1,xor^nums[i]);
+        solve(nums,i+1,xor);
+    }
+    public int subsetXORSum(int[] nums) {
+        solve(nums,0,0);
+        return ans;
     }
 }
