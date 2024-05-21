2f6d8ba263084d1a3e63c321b7448c464d13c406
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-21 20:41:56 +0530

    Time: 1 ms (57.66%) | Memory: 42.6 MB (59.10%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,37 @@
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         Arrays.sort(nums);
+//         backtrack(ans,new ArrayList<>(),nums,0);
+//         return ans;
+//     }
+//     public void backtrack(List<List<Integer>> ans,List<Integer> list,int[] nums,int start){
+//         ans.add(new ArrayList<>(list));
+
+//         for(int i=start;i<nums.length;i++){
+//             list.add(nums[i]);
+//             backtrack(ans,list,nums,i+1);
+//             list.remove(list.size()-1);
+//         }
+//     }
+// }
+
 class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        backtrack(ans,new ArrayList<>(),nums,0);
+        solve(nums,new ArrayList<>(),0);
         return ans;
     }
-    public void backtrack(List<List<Integer>> ans,List<Integer> list,int[] nums,int start){
-        ans.add(new ArrayList<>(list));
-
-        for(int i=start;i<nums.length;i++){
-            list.add(nums[i]);
-            backtrack(ans,list,nums,i+1);
-            list.remove(list.size()-1);
+    public void solve(int[] nums, ArrayList list, int i){
+        if(i==nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
         }
+        solve(nums,list,i+1);
+        list.add(nums[i]);
+        solve(nums,list,i+1);
+        list.remove(list.size()-1);
     }
+
 }
