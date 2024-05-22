5b08323f5fdce8c30eae4f4136297d962e3aa981
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-22 20:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 57 MB (34.61%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,27 +1,104 @@
+// class Solution {
+//     List<List<String>> ans=new ArrayList<>();
+//     public List<List<String>> partition(String s) {
+//         List<String> list=new ArrayList<>();
+//         solve(0,s,list);
+//         return ans;
+//     }
+//     public void solve(int idx,String s,List<String> list){
+//         if(idx==s.length()){
+//             ans.add(new ArrayList<>(list));
+//             return;
+//         }
+//         for(int i=idx;i<s.length();i++){
+//             if(check(s,idx,i)){
+//                 list.add(s.substring(idx,i+1));
+//                 solve(i+1,s,list);
+//                 list.remove(list.size()-1);
+//             }
+//         }
+//     }
+//     public boolean check(String s,int i,int j){
+//         while(i<=j){
+//             if(s.charAt(i++)!=s.charAt(j--)) return false;
+//         }
+//         return true;
+//     }
+// }
+
+import java.util.AbstractList; 
+
 class Solution {
-    List<List<String>> ans=new ArrayList<>();
+
+    private StringBuilder givenStr;
+    private int givenStrLength;
+
+    private List<List<String>> result;
+    private List<String> subsResult;
+
     public List<List<String>> partition(String s) {
-        List<String> list=new ArrayList<>();
-        solve(0,s,list);
-        return ans;
+
+        // result = new ArrayList<List<String>>();
+        givenStr = new StringBuilder(s);
+        givenStrLength = s.length();
+        subsResult = new ArrayList<String>();
+
+        // dfs(0);
+        // return result;     
+
+        return new AbstractList<List<String>>() {
+            @Override
+            public int size() {
+                init();
+                return result.size();
+            }
+
+            @Override
+            public List<String> get(int index) {
+                init();
+                return result.get(index);
+            }
+
+            private void init() {
+                if (result != null) return;
+                result = new ArrayList<List<String>>();
+                dfs(0);
+            }
+        };
+        
     }
-    public void solve(int idx,String s,List<String> list){
-        if(idx==s.length()){
-            ans.add(new ArrayList<>(list));
+
+     private void dfs(int startIndex) {
+        if (startIndex == givenStrLength) {
+            result.add(new ArrayList<String>(subsResult));
             return;
         }
-        for(int i=idx;i<s.length();i++){
-            if(check(s,idx,i)){
-                list.add(s.substring(idx,i+1));
-                solve(i+1,s,list);
-                list.remove(list.size()-1);
-            }
+
+        for (int i = startIndex; i < givenStrLength; i++) {
+            // we do not need to process further
+            // if the first part of the string (startIndex to i) is not palindrome
+            if (!isPalindrome(givenStr, startIndex, i)) continue;
+
+            // It means that first part (startIndex to i) is palindrome.
+            // Let add into the subResult
+            subsResult.add(givenStr.substring(startIndex, i + 1));
+
+            // Now let process the second part (i+1 to string length)
+            dfs(i + 1);
+
+            // Repeat the for (startIndex to i+1), so let remove from the list
+            subsResult.remove(subsResult.size() - 1);
         }
+
     }
-    public boolean check(String s,int i,int j){
-        while(i<=j){
-            if(s.charAt(i++)!=s.charAt(j--)) return false;
+
+    private boolean isPalindrome(StringBuilder s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high)) return false;
+            low++;
+            high--;
         }
         return true;
     }
+
 }
