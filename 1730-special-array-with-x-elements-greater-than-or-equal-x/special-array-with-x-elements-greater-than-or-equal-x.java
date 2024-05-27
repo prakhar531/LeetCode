3e03b8dd225c5ca9f81280de598031450dfd67ad
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-27 13:17:34 +0530

    Time: 1 ms (78.32%) | Memory: 41.3 MB (29.54%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int max=0;
+        for(int num:nums){
+            max=Math.max(num,max);
+        }
+        int arr[]=new int[max+1];
+        for(int num:nums){
+            arr[num]++;
+        }
+        for(int i=max-1;i>=0;i--){
+            arr[i]+=arr[i+1];
+        }
+        int j=0;
+        while(j<=nums.length && j<=max){
+            if(arr[j]==j) return j;
+            j++;
+        }
+        return -1;
+    }
+}
