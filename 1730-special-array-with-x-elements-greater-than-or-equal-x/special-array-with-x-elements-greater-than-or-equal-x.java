a0c42f800d752b7df9e3d3fea286d06f05384502
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-27 14:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (68.83%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,21 +1,41 @@
+// class Solution {
+//     public int specialArray(int[] nums) {
+//         int max=0;
+//         for(int num:nums){
+//             max=Math.max(num,max);
+//         }
+//         if(max==0 && nums.length>0) return -1;
+//         int arr[]=new int[max+1];
+//         for(int num:nums){
+//             arr[num]++;
+//         }
+//         for(int i=max-1;i>=0;i--){
+//             arr[i]+=arr[i+1];
+//         }
+//         int j=0;
+//         while(j<=nums.length && j<=max){
+//             if(arr[j]==j) return j;
+//             j++;
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int specialArray(int[] nums) {
-        int max=0;
-        for(int num:nums){
-            max=Math.max(num,max);
-        }
-        if(max==0 && nums.length>0) return -1;
-        int arr[]=new int[max+1];
-        for(int num:nums){
-            arr[num]++;
-        }
-        for(int i=max-1;i>=0;i--){
-            arr[i]+=arr[i+1];
-        }
-        int j=0;
-        while(j<=nums.length && j<=max){
-            if(arr[j]==j) return j;
-            j++;
+        int[] numCounts = new int[nums.length+1];
+        for(int num: nums)
+            if(num>nums.length)
+                numCounts[nums.length]++;
+            else
+                numCounts[num]++;
+        int bigNumsCount = 0;
+        for(int specialGuess = nums.length; specialGuess>0; specialGuess--){
+            bigNumsCount += numCounts[specialGuess];
+            if(bigNumsCount > specialGuess)
+                return -1;
+            if(bigNumsCount == specialGuess)
+                return specialGuess;
         }
         return -1;
     }
