235568c4097e4eb12473e865534483eacdc63724
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-29 22:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (90.36%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,41 +1,57 @@
+// class Solution {
+
+//     private void divideByTwo(StringBuilder s) {
+//         s.deleteCharAt(s.length() - 1);
+//     }
+
+//     private void addOne(StringBuilder s) {
+//         int i = s.length() - 1;
+
+//         // Iterating while the character is 1 and changing to 0
+//         while (i >= 0 && s.charAt(i) != '0') {
+//             s.setCharAt(i, '0');
+//             i--;
+//         }
+
+//         if (i < 0) {
+//             s.insert(0, '1');
+//         } else {
+//             s.setCharAt(i, '1');
+//         }
+//     }
+
+//     public int numSteps(String s) {
+//         StringBuilder str = new StringBuilder(s);
+
+//         int operations = 0;
+//         while (str.length() > 1) {
+//             int N = str.length();
+
+//             if (str.charAt(N - 1) == '0') {
+//                 divideByTwo(str);
+//             } else {
+//                 addOne(str);
+//             }
+
+//             operations++;
+//         }
+
+//         return operations;
+//     }
+// }
 class Solution {
-
-    private void divideByTwo(StringBuilder s) {
-        s.deleteCharAt(s.length() - 1);
-    }
-
-    private void addOne(StringBuilder s) {
-        int i = s.length() - 1;
-
-        // Iterating while the character is 1 and changing to 0
-        while (i >= 0 && s.charAt(i) != '0') {
-            s.setCharAt(i, '0');
-            i--;
-        }
-
-        if (i < 0) {
-            s.insert(0, '1');
-        } else {
-            s.setCharAt(i, '1');
-        }
-    }
-
     public int numSteps(String s) {
-        StringBuilder str = new StringBuilder(s);
+        int res = 0, carry = 0;
 
-        int operations = 0;
-        while (str.length() > 1) {
-            int N = str.length();
-
-            if (str.charAt(N - 1) == '0') {
-                divideByTwo(str);
-            } else {
-                addOne(str);
+        for(int i = s.length() - 1; i> 0; i--){
+            res++;
+            if(s.charAt(i) - '0' + carry == 1){
+                carry = 1;
+                res ++;
             }
 
-            operations++;
         }
 
-        return operations;
+        return res + carry;
     }
 }
