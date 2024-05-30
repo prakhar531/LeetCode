f4a346fc49daab5a0a9f6f50189636773b87c5db
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-30 16:34:55 +0530

    Time: 2 ms (77.44%) | Memory: 41.3 MB (25.64%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,18 +1,49 @@
+// class Solution {
+//     public int countTriplets(int[] arr) {
+//         int prefix[]=new int[arr.length+1];
+//         prefix[0]=0;
+//         for(int i=0;i<arr.length;i++){
+//             prefix[i+1]=prefix[i]^arr[i];
+//         }
+//         int ans=0;
+//         for(int start=0;start<prefix.length;start++){
+//             for(int end=start+1;end<prefix.length;end++){
+//                 if(prefix[start]==prefix[end]){
+//                     ans+=end-start-1;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
+
     public int countTriplets(int[] arr) {
-        int prefix[]=new int[arr.length+1];
-        prefix[0]=0;
-        for(int i=0;i<arr.length;i++){
-            prefix[i+1]=prefix[i]^arr[i];
+        int size = arr.length;
+        int count = 0;
+        int prefix = 0;
+
+        // Maps to store counts and totals of XOR values encountered
+        Map<Integer, Integer> countMap = new HashMap<>();
+        countMap.put(0, 1);
+        Map<Integer, Integer> totalMap = new HashMap<>();
+
+        // Iterating through the array
+        for (int i = 0; i < size; ++i) {
+            // Calculating XOR prefix
+            prefix ^= arr[i];
+
+            // Calculating contribution of current element to the result
+            count +=
+            countMap.getOrDefault(prefix, 0) * i -
+            totalMap.getOrDefault(prefix, 0);
+
+            // Updating total count of current XOR value
+            totalMap.put(prefix, totalMap.getOrDefault(prefix, 0) + i + 1);
+            countMap.put(prefix, countMap.getOrDefault(prefix, 0) + 1);
         }
-        int ans=0;
-        for(int start=0;start<prefix.length;start++){
-            for(int end=start+1;end<prefix.length;end++){
-                if(prefix[start]==prefix[end]){
-                    ans+=end-start-1;
-                }
-            }
-        }
-        return ans;
+
+        return count;
     }
 }
