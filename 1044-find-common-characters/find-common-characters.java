de9eec4293c4ad462cd73027e499fca5172f3b94
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-06-05 13:04:10 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (45.37%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,37 +1,70 @@
+// class Solution {
+//     public List<String> commonChars(String[] words) {
+//         int count[]=new int[26];
+//         int temp[]=new int[26];
+//         for(char ch:words[0].toCharArray()){
+//                 int idx=ch-'a';
+//                 count[idx]++;
+//         }
+
+//         for(String word:words){
+//             Arrays.fill(temp,0);
+//             for(char ch:word.toCharArray()){
+//                 int idx=ch-'a';
+//                 temp[idx]++;
+//             }
+//             for(int i=0;i<26;i++){
+//                 if(count[i]==0) continue;
+//                 if(temp[i]==0&&count[i]!=0){
+//                     count[i]=0;
+//                 }else{
+//                     count[i]=Math.min(temp[i],count[i]);
+//                 }
+
+//             }
+//         }
+
+//         List<String> ans=new ArrayList<>();
+//         for(int i=0;i<26;i++){
+//             while(count[i]!=0){
+//                 ans.add(Character.toString(i+'a'));
+//                 count[i]--;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
 class Solution {
-    public List<String> commonChars(String[] words) {
-        int count[]=new int[26];
-        int temp[]=new int[26];
-        for(char ch:words[0].toCharArray()){
-                int idx=ch-'a';
-                count[idx]++;
+   public List<String> commonChars(String[] words) {
+        int[] last = count(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            last = intersection(last, count(words[i]));
         }
-
-        for(String word:words){
-            Arrays.fill(temp,0);
-            for(char ch:word.toCharArray()){
-                int idx=ch-'a';
-                temp[idx]++;
-            }
-            for(int i=0;i<26;i++){
-                if(count[i]==0) continue;
-                if(temp[i]==0&&count[i]!=0){
-                    count[i]=0;
-                }else{
-                    count[i]=Math.min(temp[i],count[i]);
+        List<String> arr = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            if (last[i] != 0) {
+                char a = 'a';
+                a += i;
+                String s = String.valueOf(a);
+                while (last[i] > 0) {
+                    arr.add(s);
+                    last[i]--;
                 }
-
             }
         }
-
-        List<String> ans=new ArrayList<>();
-        for(int i=0;i<26;i++){
-            while(count[i]!=0){
-                ans.add(Character.toString(i+'a'));
-                count[i]--;
-            }
+        return arr;
+    }
+    int[] intersection(int[] a, int[] b) {
+        int[] t = new int[26];
+        for (int i = 0; i < 26; i++) {
+            t[i] = Math.min(a[i], b[i]);
         }
-
-        return ans;
+        return t;
+    }
+    int[] count(String str) {
+        int[] t = new int[26];
+        for (char c : str.toCharArray()) t[c - 'a']++;
+        return t;
     }
 }
