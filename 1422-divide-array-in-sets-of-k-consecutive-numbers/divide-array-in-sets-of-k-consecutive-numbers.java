5b66813e6b5d3528581245eb204462bd748aaaff
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-06-06 20:34:54 +0530

    Time: 576 ms (7.21%) | Memory: 53.1 MB (98.51%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean findsucessors(int[] hand, int groupSize, int i, int n) {
+        int f = hand[i] + 1;
+        hand[i] = -1;
+        int count = 1;
+        i += 1;
+        while (i < n && count < groupSize) {
+            if (hand[i] == f) {
+                f = hand[i] + 1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        if (count != groupSize)
+            return false;
+        else
+            return true;
+    }
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0)
+            return false;
+        Arrays.sort(hand);
+        int i = 0;
+        for (; i < n; i++) {
+            if (hand[i] >= 0) {
+                if (!findsucessors(hand, groupSize, i, n))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
