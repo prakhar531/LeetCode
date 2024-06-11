24ac99836cd02e33e0bb64c74260624227d3ac45
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-06-11 23:35:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (91.90%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,38 +1,25 @@
 class Solution {
-
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        List<Integer> remaining = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-
-        // Initialize count map with relative order elements
-        for (int value : arr2) {
-            countMap.put(value, 0);
+        int[] cnt=new int[1001];
+        for(int i:arr1){
+            cnt[i]++;
         }
-
-        // Count occurrences of elements in target array
-        for (int value : arr1) {
-            if (countMap.containsKey(value)) {
-                countMap.put(value, countMap.get(value) + 1);
-            } else {
-                remaining.add(value);
+        int[] ans=new int[arr1.length];
+        int i=0;
+        for(int n:arr2){
+            while(cnt[n]>0){
+                ans[i]=n;
+                cnt[n]--;
+                i++;
             }
         }
-
-        // Sort the remaining elements
-        Collections.sort(remaining);
-
-        // Add elements as per relative order
-        for (int value : arr2) {
-            for (int j = 0; j < countMap.get(value); j++) {
-                result.add(value);
+        for(int j=0;j<cnt.length;j++){
+            while(cnt[j]>0){
+                ans[i]=j;
+                cnt[j]--;
+                i++;
             }
         }
-
-        // Add remaining elements
-        result.addAll(remaining);
-
-        // Convert ArrayList to array
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return ans;
     }
 }
