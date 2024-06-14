9674598775397dca3a45704135d45bd4f386f28d
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-06-14 21:08:03 +0530

    Time: 37 ms (72.13%) | Memory: 60.3 MB (38.03%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -1,14 +1,37 @@
-class Solution {
-    public int minIncrementForUnique(int[] nums) {
-        Arrays.sort(nums);
-        int ans=0;
-        for(int i=1;i<nums.length;i++){
-            while(nums[i]<=nums[i-1]){
-                nums[i]++;
-                ans++;
+// class Solution {
+//     public int minIncrementForUnique(int[] nums) {
+//         Arrays.sort(nums);
+//         int ans=0;
+//         for(int i=1;i<nums.length;i++){
+//             while(nums[i]<=nums[i-1]){
+//                 nums[i]++;
+//                 ans++;
                 
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution {
+
+    public int minIncrementForUnique(int[] nums) {
+        int minIncrements = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            // Ensure each element is greater than its previous
+            if (nums[i] <= nums[i - 1]) {
+                // Add the required increment to minIncrements
+                int increment = nums[i - 1] + 1 - nums[i];
+                minIncrements += increment;
+
+                // Set the element to be greater than its previous
+                nums[i] = nums[i - 1] + 1;
             }
         }
-        return ans;
+
+        return minIncrements;
     }
 }
