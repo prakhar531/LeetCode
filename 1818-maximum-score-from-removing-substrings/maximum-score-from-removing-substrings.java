d096894489bf236fa5612328a4b8ebab70340302
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-12 23:20:39 +0530

    Time: 20 ms (100.00%) | Memory: 45.5 MB (67.68%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,34 +1,71 @@
+// class Solution {
+//     public int maximumGain(String s, int x, int y) {
+//         String high=x>y?"ab":"ba";
+//         String low=x<y?"ab":"ba";
+
+//         int highScore=x>y?x:y;
+//         int lowScore=y>x?x:y;
+
+//         String firstPass=solve(s,high);
+//         int ans=0;
+
+//         ans+=((s.length()-firstPass.length())/2)*highScore;
+//         String secondPass=solve(firstPass,low);
+//         ans+=((firstPass.length()-secondPass.length())/2)*lowScore;
+//         return ans;
+//     }
+
+//     public String solve(String s,String str){
+//         Stack<Character> stack=new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch==str.charAt(1)&& !stack.isEmpty() && stack.peek()==str.charAt(0)){
+//                 stack.pop();
+//             }else{
+//                 stack.push(ch);
+//             }
+//         }
+//         StringBuilder sb=new StringBuilder("");
+//         while(!stack.isEmpty()){
+//             sb.append(stack.pop());
+//         }
+//         return sb.reverse().toString();
+//     }
+// }
+
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        String high=x>y?"ab":"ba";
-        String low=x<y?"ab":"ba";
-
-        int highScore=x>y?x:y;
-        int lowScore=y>x?x:y;
-
-        String firstPass=solve(s,high);
-        int ans=0;
-
-        ans+=((s.length()-firstPass.length())/2)*highScore;
-        String secondPass=solve(firstPass,low);
-        ans+=((firstPass.length()-secondPass.length())/2)*lowScore;
-        return ans;
-    }
-
-    public String solve(String s,String str){
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch==str.charAt(1)&& !stack.isEmpty() && stack.peek()==str.charAt(0)){
-                stack.pop();
-            }else{
-                stack.push(ch);
+        
+        int aCount = 0;
+        int bCount = 0;
+        int lesser = Math.min(x, y);
+        int result = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 'b') {
+                result += Math.min(aCount, bCount) * lesser;
+                aCount = 0;
+                bCount = 0;
+            } else if (c == 'a') {
+                if (x < y && bCount > 0) {
+                    bCount--;
+                    result += y;
+                } else {
+                    aCount++;
+                }
+            } else {
+                if (x > y && aCount > 0) {
+                    aCount--;
+                    result += x;
+                } else {
+                    bCount++;
+                };
             }
         }
-        StringBuilder sb=new StringBuilder("");
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
+        
+        result += Math.min(aCount, bCount) * lesser;
+        
+        return result;
     }
 }
