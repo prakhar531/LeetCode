af907c6ab6b0762370868eaccbd7cb42bd94bc7b
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-17 12:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (9.41%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -13,29 +13,77 @@
  *     }
  * }
  */
-class Solution {
-    List<TreeNode> ans=new ArrayList<>();
-    public TreeNode solve(TreeNode root, HashSet<Integer> set){
-        if(root==null) return null;
+// class Solution {
+//     List<TreeNode> ans=new ArrayList<>();
+//     public TreeNode solve(TreeNode root, HashSet<Integer> set){
+//         if(root==null) return null;
         
-        root.left=solve(root.left,set);
-        root.right=solve(root.right,set);
+//         root.left=solve(root.left,set);
+//         root.right=solve(root.right,set);
 
-        if(set.contains(root.val)){
-            if(root.left!=null) ans.add(root.left);
-            if(root.right!=null) ans.add(root.right);
+//         if(set.contains(root.val)){
+//             if(root.left!=null) ans.add(root.left);
+//             if(root.right!=null) ans.add(root.right);
+//             return null;
+//         }
+
+//         return root;
+//     }
+//     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+//         HashSet<Integer> set=new HashSet<>();
+//         for(int it:to_delete){
+//             set.add(it);
+//         }
+//         solve(root,set);
+//         if(!set.contains(root.val)) ans.add(root);
+//         return ans;
+//     }
+// }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean[] s = new boolean[1001];
+    private List<TreeNode> ans = new ArrayList<>();
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        for (int x : to_delete) {
+            s[x] = true;
+        }
+        if (dfs(root) != null) {
+            ans.add(root);
+        }
+        return ans;
+    }
+
+    private TreeNode dfs(TreeNode root) {
+        if (root == null) {
             return null;
         }
-
-        return root;
-    }
-    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int it:to_delete){
-            set.add(it);
+        root.left = dfs(root.left);
+        root.right = dfs(root.right);
+        if (!s[root.val]) {
+            return root;
         }
-        solve(root,set);
-        if(!set.contains(root.val)) ans.add(root);
-        return ans;
+        if (root.left != null) {
+            ans.add(root.left);
+        }
+        if (root.right != null) {
+            ans.add(root.right);
+        }
+        return null;
     }
 }
