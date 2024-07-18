8cf3e0941626a98192d108937310fa4ce5bfed2b
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-18 16:54:43 +0530

    Time: 2 ms (95.76%) | Memory: 45 MB (30.17%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -1,35 +1,35 @@
 class Solution {
-    public int countPairs(TreeNode root, int distance) {
-        Map<TreeNode, List<TreeNode>> map = new HashMap<>();
-        List<TreeNode> leaves = new ArrayList<>();
-        findLeaves(root, new ArrayList<>(), leaves, map);
-        int res = 0;
-        for (int i = 0; i < leaves.size(); i++) {
-            for (int j = i + 1; j < leaves.size(); j++) {
-                List<TreeNode> list_i = map.get(leaves.get(i));
-                List<TreeNode> list_j = map.get(leaves.get(j));
-                for (int k = 0; k < Math.min(list_i.size(), list_j.size()); k++) {
-                    if (list_i.get(k) != list_j.get(k)) {
-                        int dist = list_i.size() - k + list_j.size() - k;
-                        if (dist <= distance) res++;
-                        break;
-                    }
-                }
+  public int countPairs(TreeNode root, int distance) {
+        int[] result= new int[1];
+        dfs(root,distance,result);
+        return result[0];
+    }
+    private int[] dfs(TreeNode node,int distance, int[] result){
+        if(node==null){
+            return new int[distance+1];
+        }
+        if(node.left==null&&node.right==null){
+            int[] leafDistance = new int[distance+1];
+            leafDistance[1]=1;
+            return leafDistance;
+        }
+
+        int[] left=dfs(node.left,distance,result);
+        int[] right= dfs(node.right,distance,result);
+
+        //calculate result
+        for(int i=1;i<=distance;i++){
+            for(int j=1;j<=distance-i;j++){
+                result[0] += left[i]*right[j];
             }
         }
-        return res;
-    }
 
-    private void findLeaves(TreeNode node, List<TreeNode> trail, List<TreeNode> leaves, Map<TreeNode, List<TreeNode>> map) {
-        if (node == null) return;
-        List<TreeNode> tmp = new ArrayList<>(trail);
-        tmp.add(node);
-        if (node.left == null && node.right == null) {
-            map.put(node, tmp);
-            leaves.add(node);
-            return;
+        int[] leafDistance = new int[distance+1];
+        for(int i=1;i<distance;i++){
+            leafDistance[i+1]= left[i]+ right[i];
         }
-        findLeaves(node.left, tmp, leaves, map);
-        findLeaves(node.right, tmp, leaves, map);
+        return leafDistance;
     }
 }
+
+
