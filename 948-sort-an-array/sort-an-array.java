a5af3dde87e08183f7465bfe160e2fb8d5a39dab
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-25 14:23:35 +0530

    Time: 4 ms (98.10%) | Memory: 57.1 MB (14.31%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,31 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        int n = max - min;
+        int[] arr = new int[n + 1];
+
+        for (int num : nums) {
+            arr[num - min]++;
+        }
+
+        int index = 0;
+
+        for (int i = 0; i <= n; i++) {
+            while (arr[i] > 0) {
+                nums[index] = min;
+                index++;
+                arr[i]--;
+            }
+
+            min++;
+        }
 
         return nums;
     }
