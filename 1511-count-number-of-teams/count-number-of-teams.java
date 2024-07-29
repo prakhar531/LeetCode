d37eaf8d76df6bff5d6c9f21128df3e22659a39a
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-29 12:12:10 +0530

    Time: 12 ms (96.06%) | Memory: 42.3 MB (62.45%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -1,40 +1,81 @@
+// class Solution {
+
+//     public int numTeams(int[] rating) {
+//         int n = rating.length;
+//         int teams = 0;
+
+//         // Tables for increasing and decreasing sequences
+//         int[][] increasingTeams = new int[n][4];
+//         int[][] decreasingTeams = new int[n][4];
+
+//         // Fill the base cases. (Each soldier is a sequence of length 1)
+//         for (int i = 0; i < n; i++) {
+//             increasingTeams[i][1] = 1;
+//             decreasingTeams[i][1] = 1;
+//         }
+
+//         // Fill the tables
+//         for (int count = 2; count <= 3; count++) {
+//             for (int i = 0; i < n; i++) {
+//                 for (int j = i + 1; j < n; j++) {
+//                     if (rating[j] > rating[i]) {
+//                         increasingTeams[j][count] +=
+//                         increasingTeams[i][count - 1];
+//                     }
+//                     if (rating[j] < rating[i]) {
+//                         decreasingTeams[j][count] +=
+//                         decreasingTeams[i][count - 1];
+//                     }
+//                 }
+//             }
+//         }
+
+//         // Sum up the results (All sequences of length 3)
+//         for (int i = 0; i < n; i++) {
+//             teams += increasingTeams[i][3] + decreasingTeams[i][3];
+//         }
+
+//         return teams;
+//     }
+// }
+
 class Solution {
 
     public int numTeams(int[] rating) {
         int n = rating.length;
         int teams = 0;
 
-        // Tables for increasing and decreasing sequences
-        int[][] increasingTeams = new int[n][4];
-        int[][] decreasingTeams = new int[n][4];
+        // Iterate through each soldier as the middle soldier
+        for (int mid = 0; mid < n; mid++) {
+            int leftSmaller = 0;
+            int rightLarger = 0;
 
-        // Fill the base cases. (Each soldier is a sequence of length 1)
-        for (int i = 0; i < n; i++) {
-            increasingTeams[i][1] = 1;
-            decreasingTeams[i][1] = 1;
-        }
-
-        // Fill the tables
-        for (int count = 2; count <= 3; count++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    if (rating[j] > rating[i]) {
-                        increasingTeams[j][count] +=
-                        increasingTeams[i][count - 1];
-                    }
-                    if (rating[j] < rating[i]) {
-                        decreasingTeams[j][count] +=
-                        decreasingTeams[i][count - 1];
-                    }
+            // Count soldiers with smaller rating on the left side of the current soldier
+            for (int left = mid - 1; left >= 0; left--) {
+                if (rating[left] < rating[mid]) {
+                    leftSmaller++;
                 }
             }
+
+            // Count soldiers with larger ratings on the right side of the current soldier
+            for (int right = mid + 1; right < n; right++) {
+                if (rating[right] > rating[mid]) {
+                    rightLarger++;
+                }
+            }
+
+            // Calculate and add the number of ascending rating teams (small-mid-large)
+            teams += leftSmaller * rightLarger;
+
+            // Calculate soldiers with larger ratings on the left and smaller ratings on the right
+            int leftLarger = mid - leftSmaller;
+            int rightSmaller = n - mid - 1 - rightLarger;
+
+            // Calculate and add the number of descending rating teams (large-mid-small)
+            teams += leftLarger * rightSmaller;
         }
 
-        // Sum up the results (All sequences of length 3)
-        for (int i = 0; i < n; i++) {
-            teams += increasingTeams[i][3] + decreasingTeams[i][3];
-        }
-
+        // Return the total number of valid teams
         return teams;
     }
 }
