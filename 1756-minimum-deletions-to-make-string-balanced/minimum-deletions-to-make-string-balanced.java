ee4abfdd583ccea41b1acdc1b3aef8baed65eb37
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-30 12:53:18 +0530

    Time: 12 ms (100.00%) | Memory: 45.2 MB (77.16%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,47 +1,68 @@
+// // class Solution {
+
+// //     public int minimumDeletions(String s) {
+// //         int n = s.length();
+// //         Stack<Character> charStack = new Stack<>();
+// //         int deleteCount = 0;
+
+// //         // Iterate through each character in the string
+// //         for (int i = 0; i < n; i++) {
+// //             // If stack is not empty, top of stack is 'b',
+// //             // and current char is 'a'
+// //             if (
+// //                 !charStack.isEmpty() &&
+// //                 charStack.peek() == 'b' &&
+// //                 s.charAt(i) == 'a'
+// //             ) {
+// //                 charStack.pop(); // Remove 'b' from stack
+// //                 deleteCount++; // Increment deletion count
+// //             } else {
+// //                 charStack.push(s.charAt(i)); // Push current character onto stack
+// //             }
+// //         }
+
+// //         return deleteCount;
+// //     }
+// // }
+
 // class Solution {
 
 //     public int minimumDeletions(String s) {
 //         int n = s.length();
-//         Stack<Character> charStack = new Stack<>();
-//         int deleteCount = 0;
+//         int minDeletions = 0;
+//         int bCount = 0;
 
-//         // Iterate through each character in the string
+//         // minDeletions variable represents dp[i]
 //         for (int i = 0; i < n; i++) {
-//             // If stack is not empty, top of stack is 'b',
-//             // and current char is 'a'
-//             if (
-//                 !charStack.isEmpty() &&
-//                 charStack.peek() == 'b' &&
-//                 s.charAt(i) == 'a'
-//             ) {
-//                 charStack.pop(); // Remove 'b' from stack
-//                 deleteCount++; // Increment deletion count
+//             if (s.charAt(i) == 'b') {
+//                 bCount++;
 //             } else {
-//                 charStack.push(s.charAt(i)); // Push current character onto stack
+//                 // Two cases: remove 'a' or keep 'a'
+//                 minDeletions = Math.min(minDeletions + 1, bCount);
 //             }
 //         }
 
-//         return deleteCount;
+//         return minDeletions;
 //     }
 // }
 
 class Solution {
-
     public int minimumDeletions(String s) {
-        int n = s.length();
-        int minDeletions = 0;
+        char[] letters = s.toCharArray();
+        int aCount = 0;
         int bCount = 0;
+        int output = Integer.MAX_VALUE;
+        for (int i = 0; i < letters.length; i++)
+            aCount += ('b' - letters[i]);
 
-        // minDeletions variable represents dp[i]
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'b') {
-                bCount++;
-            } else {
-                // Two cases: remove 'a' or keep 'a'
-                minDeletions = Math.min(minDeletions + 1, bCount);
-            }
+        for (int i = 0; i < s.length(); i++)
+        {
+            output = Math.min(output, aCount + bCount);
+            aCount -= ('b' - letters[i]);
+            bCount += (letters[i] - 'a');
         }
 
-        return minDeletions;
+        output = Math.min(output, aCount + bCount);
+        return output;
     }
 }
