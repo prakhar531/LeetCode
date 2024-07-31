33e3a13e05e39e0d578302a99dbde609a196bb89
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-07-31 11:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (88.15%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.java
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.java
@@ -61,33 +61,51 @@
 //     }
 // }
 
+// class Solution {
+
+//     public int minHeightShelves(int[][] books, int shelfWidth) {
+//         // dp[i] = minimum height of bookcase containing all books up to and
+//         // excluding book i
+//         int[] dp = new int[books.length + 1];
+
+//         // base cases
+//         dp[0] = 0;
+//         dp[1] = books[0][1];
+
+//         for (int i = 2; i <= books.length; i++) {
+//             // new shelf built to hold current book
+//             int remainingShelfWidth = shelfWidth - books[i - 1][0];
+//             int maxHeight = books[i - 1][1];
+//             dp[i] = books[i - 1][1] + dp[i - 1];
+
+//             int j = i - 1;
+//             // calculate the height when previous books are added onto a new shelf
+//             while (j > 0 && remainingShelfWidth - books[j - 1][0] >= 0) {
+//                 maxHeight = Math.max(maxHeight, books[j - 1][1]);
+//                 remainingShelfWidth -= books[j - 1][0];
+//                 dp[i] = Math.min(dp[i], maxHeight + dp[j - 1]);
+//                 j--;
+//             }
+//         }
+
+//         return dp[books.length];
+//     }
+// }
+
 class Solution {
-
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        // dp[i] = minimum height of bookcase containing all books up to and
-        // excluding book i
         int[] dp = new int[books.length + 1];
-
-        // base cases
-        dp[0] = 0;
-        dp[1] = books[0][1];
-
-        for (int i = 2; i <= books.length; i++) {
-            // new shelf built to hold current book
-            int remainingShelfWidth = shelfWidth - books[i - 1][0];
-            int maxHeight = books[i - 1][1];
-            dp[i] = books[i - 1][1] + dp[i - 1];
-
-            int j = i - 1;
-            // calculate the height when previous books are added onto a new shelf
-            while (j > 0 && remainingShelfWidth - books[j - 1][0] >= 0) {
-                maxHeight = Math.max(maxHeight, books[j - 1][1]);
-                remainingShelfWidth -= books[j - 1][0];
-                dp[i] = Math.min(dp[i], maxHeight + dp[j - 1]);
-                j--;
+        dp[books.length] = 0;
+        for (int i = books.length - 1; i >= 0; i--) {
+            int height = books[i][1];
+            int width = books[i][0];
+            dp[i] = height + dp[i + 1];
+            for (int j = i + 1; j < books.length && (width+books[j][0] <= shelfWidth); j++) {
+                height = Math.max(height, books[j][1]);
+                width += books[j][0];
+                dp[i] = Math.min(dp[i], height + dp[j+1]);
             }
         }
-
-        return dp[books.length];
+        return dp[0];
     }
 }
