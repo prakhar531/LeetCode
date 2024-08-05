ef1b7d45139b7b95020ba28cf0a3ddd5f5f7eb79
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-08-06 00:03:18 +0530

    Time: 4 ms (100.00%) | Memory: 44.4 MB (24.92%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -1,14 +1,47 @@
+// class Solution {
+//     public String kthDistinct(String[] arr, int k) {
+//         HashMap<String,Integer> map=new HashMap<>();
+//         for(String str:arr){
+//             map.put(str,map.getOrDefault(str,0)+1);
+//         }
+//         int count=0;
+//         for(String str:arr){
+//             if(map.get(str)==1){
+//                 count++;
+//                 if(count==k) return str;
+//             }
+//         }
+//         return "";
+//     }
+// }
+
 class Solution {
     public String kthDistinct(String[] arr, int k) {
-        HashMap<String,Integer> map=new HashMap<>();
-        for(String str:arr){
-            map.put(str,map.getOrDefault(str,0)+1);
+        List<String> uniqueValInArray = new LinkedList<>();
+        int totalDistinctVal = 0;
+        Set<String> nonDistinctSet = new HashSet<>();
+        Set<String> distinctSet = new HashSet<>();
+        for(String s:arr) {
+            if(!nonDistinctSet.contains(s)) {
+                if(!distinctSet.contains(s)) {
+                    distinctSet.add(s);
+                    totalDistinctVal++;
+                } else {
+                    nonDistinctSet.add(s);
+                    distinctSet.remove(s);
+                    totalDistinctVal--;
+                }
+            }
         }
-        int count=0;
-        for(String str:arr){
-            if(map.get(str)==1){
-                count++;
-                if(count==k) return str;
+        if(totalDistinctVal<k) {
+            return "";
+        }
+        for(String s:arr) {
+            if(distinctSet.contains(s)){
+                k--;
+                if(k==0){
+                    return s;
+                }
             }
         }
         return "";
