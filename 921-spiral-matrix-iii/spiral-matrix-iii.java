54e445d71c0f647651c86f4e2b82ebddeff115e5
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-08-08 10:37:59 +0530

    Time: 5 ms (47.43%) | Memory: 45.1 MB (72.43%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,25 +1,62 @@
+// class Solution {
+//     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+//         int dir[][]=new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+//         int ans[][]=new int[rows*cols][2];
+//         int step=1;
+//         int direction=0;
+//         int idx=0;
+//         while(idx<rows*cols){
+//             for(int i=0;i<2;i++){
+//                 for(int j=0;j<step;j++){
+//                     if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
+//                         ans[idx][0]=rStart;
+//                         ans[idx][1]=cStart;
+//                         idx++;
+//                     }
+//                     rStart+=dir[direction][0];
+//                     cStart+=dir[direction][1];
+//                 }
+//                 direction=(direction+1)%4;
+//             }
+//             step++;
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
+
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int dir[][]=new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
-        int ans[][]=new int[rows*cols][2];
-        int step=1;
-        int direction=0;
-        int idx=0;
-        while(idx<rows*cols){
-            for(int i=0;i<2;i++){
-                for(int j=0;j<step;j++){
-                    if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
-                        ans[idx][0]=rStart;
-                        ans[idx][1]=cStart;
-                        idx++;
+        // Store all possible directions in an array.
+        int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        int[][] traversed = new int[rows * cols][2];
+        int idx = 0;
+
+        // Initial step size is 1, value of d represents the current direction.
+        for (int step = 1, direction = 0; idx < rows * cols;) {
+            // direction = 0 -> East, direction = 1 -> South
+            // direction = 2 -> West, direction = 3 -> North
+            for (int i = 0; i < 2; ++i) {
+                for (int j = 0; j < step; ++j) {
+                    // Validate the current position
+                    if (
+                        rStart >= 0 &&
+                        rStart < rows &&
+                        cStart >= 0 &&
+                        cStart < cols
+                    ) {
+                        traversed[idx][0] = rStart;
+                        traversed[idx][1] = cStart;
+                        ++idx;
                     }
-                    rStart+=dir[direction][0];
-                    cStart+=dir[direction][1];
+                    // Make changes to the current position.
+                    rStart += dir[direction][0];
+                    cStart += dir[direction][1];
                 }
-                direction=(direction+1)%4;
+
+                direction = (direction + 1) % 4;
             }
-            step++;
+            ++step;
         }
-        return ans;
+        return traversed;
     }
 }
