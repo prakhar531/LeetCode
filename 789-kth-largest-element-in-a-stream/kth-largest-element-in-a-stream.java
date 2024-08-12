a4c6ea2588e119ef22beb066f948cca3e1fdd0ec
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-08-12 23:42:10 +0530

    Time: 10 ms (99.98%) | Memory: 47.2 MB (24.97%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,41 +1,68 @@
 class KthLargest {
 
-    List<Integer> stream;
-    int k;
+    private int size = 0;
+    private final int k;
+    private final int[] heap;
 
     public KthLargest(int k, int[] nums) {
-        stream = new ArrayList<Integer>(nums.length);
         this.k = k;
-
+        this.heap = new int[k + 1];
         for (int num : nums) {
-            stream.add(num);
+            add(num);
         }
-
-        Collections.sort(stream);
     }
 
     public int add(int val) {
-        int index = getIndex(val);
-        // Add val to correct position
-        stream.add(index, val);
-        return stream.get(stream.size() - k);
+        
+        if (size == k) {
+            if (val < heap[1]) {
+                return heap[1];
+            }
+            pop();
+        }
+
+        heap[++size] = val;
+
+        int index = size;
+        int parent = size / 2;
+
+        while (heap[parent] > heap[index] && index > 1) {
+
+            int temp = heap[index];
+            heap[index] = heap[parent];
+            heap[parent] = temp;
+
+            index = parent;
+            parent = parent / 2;
+        }
+
+        return heap[1];
     }
 
-    private int getIndex(int val) {
-        int left = 0;
-        int right = stream.size() - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int midElement = stream.get(mid);
-            if (midElement == val) return mid;
-            if (midElement > val) {
-                // Go to left half
-                right = mid - 1;
+    public void pop() {
+        heap[1] = heap[size--];
+
+        int index = 1;
+        while (index <= size / 2) {
+            int left = index * 2;
+            int right = (index * 2) + 1;
+
+            if (heap[index] > heap[left] || heap[index] > heap[right]) {
+                if (heap[left] < heap[right]) {
+                    int temp = heap[left];
+                    heap[left] = heap[index];
+                    heap[index] = temp;
+                    index = left;
+                } else {
+                    int temp = heap[right];
+                    heap[right] = heap[index];
+                    heap[index] = temp;
+                    index = right;
+                }
             } else {
-                // Go to right half
-                left = mid + 1;
+                break;
             }
         }
-        return left;
     }
+
 }
