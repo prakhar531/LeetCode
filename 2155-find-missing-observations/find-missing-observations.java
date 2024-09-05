6538c5bcd1ffa9acb980e34fcc7276b4cba10723
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-09-05 10:15:05 +0530

    Time: 3 ms (98.03%) | Memory: 59.8 MB (77.38%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.java
+++ 2155-find-missing-observations/find-missing-observations.java
@@ -1,21 +1,47 @@
-class Solution {
-    public int[] missingRolls(int[] rolls, int mean, int n) {
-        int m=rolls.length;
-        int rollSum=0;
-        for(int it:rolls) rollSum+=it;
-        int sum=(n+m)*mean-rollSum;
-        int ans[]=new int[n];
-        if(sum>n*6||sum<n) return new int[]{};
+// class Solution {
+//     public int[] missingRolls(int[] rolls, int mean, int n) {
+//         int m=rolls.length;
+//         int rollSum=0;
+//         for(int it:rolls) rollSum+=it;
+//         int sum=(n+m)*mean-rollSum;
+//         int ans[]=new int[n];
+//         if(sum>n*6||sum<n) return new int[]{};
 
-        int avg=sum/n;
-        int rem=sum%n;
-        for(int i=0;i<n;i++){
-            ans[i]=avg;
-            if(rem!=0){
-                ans[i]++;
-                rem--;
-            }
+//         int avg=sum/n;
+//         int rem=sum%n;
+//         for(int i=0;i<n;i++){
+//             ans[i]=avg;
+//             if(rem!=0){
+//                 ans[i]++;
+//                 rem--;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+class Solution {
+
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int[] ret = new int[n];
+        int sum = 0;
+        for (int i : rolls) {
+            sum += i;
         }
-        return ans;
+        int missingSum = mean * (n + rolls.length) - sum;
+        if (missingSum > 6*n || missingSum < n) {
+            return new int[]{};
+        }
+        int expectedAvg = missingSum/n, remainder = missingSum%n;
+
+        for (int i = 0; i < remainder; i++) {
+            ret[i] = expectedAvg + 1;
+        }
+        for (int i = remainder; i < n;i++) {
+            ret[i] = expectedAvg;
+        }
+        return ret;
     }
 }
