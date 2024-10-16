ea4696f13e4c6d1523bcab7f62b6074932af3272
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-10-16 13:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (91.62%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.java
+++ 1304-longest-happy-string/longest-happy-string.java
@@ -1,62 +1,44 @@
 class Solution {
 
     public String longestDiverseString(int a, int b, int c) {
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) ->
-            (y.count - x.count)
-        );
-        // Add the counts of a, b and c in priority queue.
-        if (a > 0) {
-            pq.add(new Pair(a, 'a'));
-        }
-
-        if (b > 0) {
-            pq.add(new Pair(b, 'b'));
-        }
-
-        if (c > 0) {
-            pq.add(new Pair(c, 'c'));
-        }
-
+        int curra = 0, currb = 0, currc = 0;
+        // Maximum total iterations possible is given by the sum of a, b and c.
+        int totalIterations = a + b + c;
         StringBuilder ans = new StringBuilder();
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            int count = p.count;
-            char character = p.character;
-            // If three consecutive characters exists, pick the second most
-            // frequent character.
+
+        for (int i = 0; i < totalIterations; i++) {
             if (
-                ans.length() >= 2 &&
-                ans.charAt(ans.length() - 1) == p.character &&
-                ans.charAt(ans.length() - 2) == p.character
+                (a >= b && a >= c && curra != 2) ||
+                (a > 0 && (currb == 2 || currc == 2))
             ) {
-                if (pq.isEmpty()) break;
-
-                Pair temp = pq.poll();
-                ans.append(temp.character);
-                if (temp.count - 1 > 0) {
-                    pq.add(new Pair(temp.count - 1, temp.character));
-                }
-            } else {
-                count--;
-                ans.append(character);
-            }
-
-            // If count is greater than zero, add it to priority queue.
-            if (count > 0) {
-                pq.add(new Pair(count, character));
+                // If 'a' is maximum and it's streak is less than 2, or if streak of 'b' or 'c' is 2, then 'a' will be the next character.
+                ans.append('a');
+                a--;
+                curra++;
+                currb = 0;
+                currc = 0;
+            } else if (
+                (b >= a && b >= c && currb != 2) ||
+                (b > 0 && (currc == 2 || curra == 2))
+            ) {
+                // If 'b' is maximum and it's streak is less than 2, or if streak of 'a' or 'c' is 2, then 'b' will be the next character.
+                ans.append('b');
+                b--;
+                currb++;
+                curra = 0;
+                currc = 0;
+            } else if (
+                (c >= a && c >= b && currc != 2) ||
+                (c > 0 && (curra == 2 || currb == 2))
+            ) {
+                // If 'c' is maximum and it's streak is less than 2, or if streak of 'a' or 'b' is 2, then 'c' will be the next character.
+                ans.append('c');
+                c--;
+                currc++;
+                curra = 0;
+                currb = 0;
             }
         }
         return ans.toString();
-    }
-
-    class Pair {
-
-        int count;
-        char character;
-
-        Pair(int count, char character) {
-            this.count = count;
-            this.character = character;
-        }
     }
 }
